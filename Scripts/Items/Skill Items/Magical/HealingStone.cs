ef7815086bfa829396af14bbd36aa7304d3eea85
Ravenwolfe <brian@tindomorgans.com>
2014-01-23 21:14:09 -0600

    Fix to Healing Stone for OSI accuracy

--- Scripts/Items/Skill Items/Magical/HealingStone.cs
+++ Scripts/Items/Skill Items/Magical/HealingStone.cs
@@ -5,21 +5,42 @@ namespace Server.Items
     public class HealingStone : Item
     {
         private readonly Mobile m_Caster;
-        private readonly int m_Amount;
+		private int m_Amount;
+		
+        [CommandProperty(AccessLevel.GameMaster)]
+		public int HealPoints
+		{
+			get
+			{
+				return this.m_Amount;
+			}
+			set
+			{
+				this.m_Amount = value;
+			}
+		}
+		
         [Constructable]
         public HealingStone(Mobile caster, int amount)
             : base(0x4078)
         {
             this.m_Caster = caster;
             this.m_Amount = amount;
-        }
+		}
 
         public HealingStone(Serial serial)
             : base(serial)
         {
         }
 
-        public override void OnDoubleClick(Mobile from)
+       public override void AddNameProperties(ObjectPropertyList list)
+		{
+			base.AddNameProperties(list);
+
+			list.Add("Healing Points: {0}", m_Amount);
+		}
+		
+		public override void OnDoubleClick(Mobile from)
         {
             if (!from.InRange(this.GetWorldLocation(), 1))
             {
@@ -41,11 +62,28 @@ namespace Server.Items
             {
                 from.SendLocalizedMessage(1080116); // You must have a free hand to use a Healing Stone.
             }
+			else if (from.Hits >= from.HitsMax)
+						from.SendMessage("You are at full health,");
+					
             else if (from.BeginAction(typeof(BaseHealPotion)))
             {
-                from.Heal(Utility.RandomMinMax(BasePotion.Scale(from, 13), BasePotion.Scale(from, 16)));
-                this.Consume();
-                Timer.DelayCall(TimeSpan.FromSeconds(8.0), new TimerStateCallback(ReleaseHealLock), from);
+                int healamount = Utility.RandomMinMax(BasePotion.Scale(from, 25), BasePotion.Scale(from, 35));
+					
+				if (healamount > m_Amount)
+					healamount = m_Amount;
+					
+				from.Heal (healamount);
+					
+				m_Amount -= healamount;
+					
+				if (m_Amount <= 0)
+					this.Consume();
+					
+				this.InvalidateProperties();	
+					
+				Timer.DelayCall(TimeSpan.FromSeconds(8.0), new TimerStateCallback(ReleaseHealLock), from);
+				
+				
             }
             else
                 from.SendLocalizedMessage(1095172); // You must wait a few seconds before using another Healing Stone.
@@ -66,12 +104,16 @@ namespace Server.Items
         {
             base.Serialize(writer);
             writer.Write((int)0);
+			
+			writer.Write(this.m_Amount);
         }
 
         public override void Deserialize(GenericReader reader)
         {
             base.Deserialize(reader);
             int version = reader.ReadInt();
+			
+			this.m_Amount = reader.ReadInt();
         }
 
         private static void ReleaseHealLock(object state)
