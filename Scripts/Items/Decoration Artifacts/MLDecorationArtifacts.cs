45da465b0f9a29e8e346b93638ceff927206c418
James Black <james@james-black.com>
2014-03-06 13:45:58 -0700

    Adds the Ship Model of the H.M.S Cape

---
+++ Scripts/Items/Decoration Artifacts/MLDecorationArtifacts.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Server;
+
+namespace Server.Items 
+{
+    public class AncientShipModelOfTheHMSCape : Item
+    {
+        public override int LabelNumber { get { return 1063476; } }
+
+        [Constructable]
+        public AncientShipModelOfTheHMSCape()
+            : base(0x14F3)
+        {
+            Name = "an Ancient Ship Model Of The HMS Cape";
+            Hue = 0x37B;
+        }
+
+        public AncientShipModelOfTheHMSCape(Serial serial)
+            : base(serial)
+        {
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+
+            writer.Write((int)0);
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+
+            int version = reader.ReadInt();
+        }
+    }
+}
