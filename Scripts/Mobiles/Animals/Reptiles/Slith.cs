8e58b67f5493c1d7f04c5247c70a834d4956b368
James Black <james@james-black.com>
2014-03-09 01:38:29 -0700

    Fixes to the Slith to be more like OSI (Fix Courtesy of xGOOBERx)

--- Scripts/Mobiles/Animals/Reptiles/Slith.cs
+++ Scripts/Mobiles/Animals/Reptiles/Slith.cs
@@ -12,24 +12,24 @@ namespace Server.Mobiles
             this.Name = "a slith";
             this.Body = 734; 
 
-            this.SetStr(129, 136);
-            this.SetDex(72, 75);
-            this.SetInt(12, 13);
+            this.SetStr(133, 146);
+            this.SetDex(59, 67);
+            this.SetInt(12, 20);
 
-            this.SetHits(84, 85);
+            this.SetHits(84, 94);
 
             this.SetDamage(6, 24);
 
             this.SetDamageType(ResistanceType.Physical, 100);
 
-            this.SetResistance(ResistanceType.Physical, 35, 40);
-            this.SetResistance(ResistanceType.Fire, 35, 45);
+            this.SetResistance(ResistanceType.Physical, 35, 45);
+            this.SetResistance(ResistanceType.Fire, 30, 45);
             this.SetResistance(ResistanceType.Poison, 25, 35);
-            this.SetResistance(ResistanceType.Energy, 25, 30);
+            this.SetResistance(ResistanceType.Energy, 25, 35);
 
-            this.SetSkill(SkillName.MagicResist, 59.1, 63.5);
-            this.SetSkill(SkillName.Tactics, 74.6, 76.4);
-            this.SetSkill(SkillName.Wrestling, 62.0, 77.1);
+            this.SetSkill(SkillName.MagicResist, 59.2, 67.9);
+            this.SetSkill(SkillName.Tactics, 66.8, 78.4);
+            this.SetSkill(SkillName.Wrestling, 63.1, 77.6);
 
             this.Tamable = true;
             this.ControlSlots = 1;
