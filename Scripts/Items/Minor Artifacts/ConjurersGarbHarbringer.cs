9239915495c3fe90036bf4004ff0bf6e2c8b98b2
ruaduck <sales@uoguyz.net>
2014-03-17 01:34:18 +0000

    Fixes Conjurer items to be more OSI Like.

---
+++ Scripts/Items/Minor Artifacts/ConjurersGarbHarbringer.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace Server.Items
+{
+    [Flipable(0x1F03, 0x1F04)]
+    public class ConjureresGarbHarbringer : BaseOuterTorso
+    {
+        [Constructable]
+        public ConjureresGarbHarbringer()
+            : base(0x1F03, 0x486)
+        {
+			this.Hue = 0x4AA;
+			this.Name = "Conjurer's Garb";
+            this.Weight = 3.0;
+			this.Attributes.DefendChance = 5;
+			this.Attributes.RegenMana = 2;
+			// TODO: Supports arcane?
+        }
+
+        public ConjureresGarbHarbringer(Serial serial)
+            : base(serial)
+        {
+        }
+
+        // Conjurer's Garb on Harbringer
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+
+            writer.WriteEncodedInt(0); // version
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+
+            int version = reader.ReadEncodedInt();
+        }
+    }
+}
