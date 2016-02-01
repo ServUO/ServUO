3868e4e322517645ed40fea46ac958d5f2bf2d24
Norman B. Lancaster <qbradq@gmail.com>
2016-02-01 12:01:21 -0600

    Fixes #353

--- Scripts/Skills/Hiding.cs
+++ Scripts/Skills/Hiding.cs
@@ -100,6 +100,7 @@ namespace Server.SkillHandlers
                 {
                     m.Hidden = true;
                     m.Warmode = false;
+					Server.Spells.Sixth.InvisibilitySpell.RemoveTimer(m);
                     m.LocalOverheadMessage(MessageType.Regular, 0x1F4, 501240); // You have hidden yourself well.
                 }
                 else
