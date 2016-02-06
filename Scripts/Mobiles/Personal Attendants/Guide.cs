50b2406dec2c8d2b3bd060085c7faa7eed60834e
Scott Taylor <cstaylor8311@gmail.com>
2016-02-05 22:54:15 -0600

    Fix for Scripts/Mobiles/Personal Attendants/Guide.cs not finding /Data/Guide/Definitions.cfg (linux only?)

--- Scripts/Mobiles/Personal Attendants/Guide.cs
+++ Scripts/Mobiles/Personal Attendants/Guide.cs
@@ -16,6 +16,7 @@ namespace Server.Mobiles
         private static readonly List<int> m_ShopDefinitions = new List<int>();
         private static readonly char[] m_Separators = new char[] { '\t', ' ' };
         private static readonly string m_Delimiter = "--------------------------------------------------------------------------";
+        private static readonly string path_Delimiter = Path.DirectorySeparatorChar.ToString();
         public static void LogMessage(string line)
         {
             try
@@ -58,7 +59,7 @@ namespace Server.Mobiles
 
             try
             {
-                using (FileStream stream = File.OpenRead("Data\\Guide\\Definitions.cfg"))
+                using (FileStream stream = File.OpenRead(String.Join(path_Delimiter, "Data", "Guide", "Definitions.cfg")))
                     using (StreamReader reader = new StreamReader(stream))
                     {
                         while (!reader.EndOfStream)
@@ -75,7 +76,7 @@ namespace Server.Mobiles
                         }
                     }
 
-                foreach (string file in Directory.GetFiles("Data\\Guide\\", "*.graph"))
+                foreach (string file in Directory.GetFiles(String.Join(path_Delimiter, "Data", "Guide"), "*.graph"))
                 {
                     using (FileStream stream = File.OpenRead(file))
                         using (StreamReader reader = new StreamReader(stream))
