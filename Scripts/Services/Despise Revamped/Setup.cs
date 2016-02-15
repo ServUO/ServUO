d99135ea91234272f3099b9722891ab035aa1fdc
Norman B. Lancaster <qbradq@gmail.com>
2016-02-15 17:22:15 -0600

    Fixes #362

--- Scripts/Services/Despise Revamped/Setup.cs
+++ Scripts/Services/Despise Revamped/Setup.cs
@@ -11,8 +11,14 @@ namespace Server.Engines.Despise
     {
         public static void Initialize()
         {
-            if(Core.HS)
-                CommandSystem.Register("SetupDespise", AccessLevel.GameMaster, new CommandEventHandler(SetupDespise_OnCommand));
+            CommandSystem.Register("SetupDespise", AccessLevel.GameMaster, new CommandEventHandler(SetupDespise_OnCommand));
+            CommandSystem.Register("DeleteDespise", AccessLevel.GameMaster, new CommandEventHandler(DeleteDespise_OnCommand));
+        }
+
+        private static void DeleteDespise_OnCommand(CommandEventArgs e)
+        {
+            WeakEntityCollection.Delete("despise");
+            DespiseController.Instance = null;
         }
 
         public static void SetupDespise_OnCommand(CommandEventArgs e)
@@ -20,16 +26,21 @@ namespace Server.Engines.Despise
             if (DespiseController.Instance == null)
             {
                 DespiseController controller = new DespiseController();
+                WeakEntityCollection.Add("despise", controller);
                 controller.MoveToWorld(new Point3D(5571, 626, 30), Map.Trammel);
 
                 DespiseAnkh ankh = new DespiseAnkh(Alignment.Good);
+                WeakEntityCollection.Add("despise", ankh);
                 ankh.MoveToWorld(new Point3D(5474, 525, 79), Map.Trammel);
 
                 ankh = new DespiseAnkh(Alignment.Evil);
+                WeakEntityCollection.Add("despise", ankh);
                 ankh.MoveToWorld(new Point3D(5472, 754, 10), Map.Trammel);
 
                 Moongate gate1 = new Moongate(false);
                 Moongate gate2 = new Moongate(false);
+                WeakEntityCollection.Add("despise", gate1);
+                WeakEntityCollection.Add("despise", gate2);
 
                 //Gate1
                 gate1.MoveToWorld(new Point3D(5475, 735, 5), Map.Trammel);
@@ -39,6 +50,8 @@ namespace Server.Engines.Despise
 
                 gate1 = new Moongate(false);
                 gate2 = new Moongate(false);
+                WeakEntityCollection.Add("despise", gate1);
+                WeakEntityCollection.Add("despise", gate2);
 
                 //Gate2
                 gate1.MoveToWorld(new Point3D(5459, 674, 20), Map.Trammel);
@@ -48,6 +61,8 @@ namespace Server.Engines.Despise
 
                 gate1 = new Moongate(false);
                 gate2 = new Moongate(false);
+                WeakEntityCollection.Add("despise", gate1);
+                WeakEntityCollection.Add("despise", gate2);
 
                 //Gate3
                 gate1.MoveToWorld(new Point3D(5388, 753, 5), Map.Trammel);
@@ -61,6 +76,7 @@ namespace Server.Engines.Despise
 
                 //Wisp
                 MysteriousWisp wisp = new MysteriousWisp();
+                WeakEntityCollection.Add("despise", wisp);
                 wisp.MoveToWorld(new Point3D(1303, 1088, 0), Map.Trammel);
 
                 foreach (Item item in eable)
@@ -70,6 +86,7 @@ namespace Server.Engines.Despise
                         Teleporter old = (Teleporter)item;
 
                         tele = new DespiseTeleporter();
+                        WeakEntityCollection.Add("despise", tele);
                         tele.PointDest = old.PointDest;
                         tele.MapDest = old.MapDest;
                         tele.MoveToWorld(old.Location, old.Map);
@@ -79,10 +96,10 @@ namespace Server.Engines.Despise
                 }
                 eable.Free();
 
-                e.Mobile.SendMessage("Despise Revamped setup! Don't forget to setup mob spawners an activate it!");
+                e.Mobile.SendMessage("Despise setup complete");
             }
             else
-                e.Mobile.SendMessage("This has already been setup.");
+                e.Mobile.SendMessage("Despise appears to already be setup");
         }
 
         private static void HueGates(Moongate one, Moongate two)
