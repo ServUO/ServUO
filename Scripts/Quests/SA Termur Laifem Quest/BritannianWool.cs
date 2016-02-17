dd67b0d024cff606a0f3a030e61e84cafb78a2ef
Norman B. Lancaster <qbradq@gmail.com>
2016-02-16 20:48:11 -0600

    Fixes #288

--- Scripts/Quests/SA Termur Laifem Quest/BritannianWool.cs
+++ Scripts/Quests/SA Termur Laifem Quest/BritannianWool.cs
@@ -1,123 +1,51 @@
+/*
+ * Created by SharpDevelop.
+ * User: Sharon
+ * Date: 5/27/2015
+ * Time: 4:54 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
 using System;
 using Server.Items;
 using Server.Targeting;
 
 namespace Server.Items
 {
-	public class BritannianWool : Item //, IDyable
-	{
-		[Constructable]
-		public BritannianWool() : this( 1 )
-		{
-		}
+    public class BritannianWool : Wool
+    {
+        public override int LabelNumber { get { return 1113242; } } // Britannian wool
 
-		[Constructable]
-		public BritannianWool( int amount ) : base( 0xDF8 )
-		{
-			Stackable = true;
-            Name = "Britannian Wool";
-            Hue = 927;
-			Weight = 4.0;
-			Amount = amount;
-		}
+        [Constructable]
+        public BritannianWool() : this(1)
+        {
+        }
 
-        public BritannianWool(Serial serial)
-            : base(serial)
-		{
-		}
+        [Constructable]
+        public BritannianWool(int amount) : base(0x101F)
+        {
+            Stackable = true;
+            Hue = 962;//dark grey
+            Weight = 4.0;
+            Amount = amount;
+        }
 
-		public override void Serialize( GenericWriter writer )
-		{
-			base.Serialize( writer );
+        public BritannianWool(Serial serial) : base(serial)
+        {
+        }
 
-			writer.Write( (int) 0 ); // version
-		}
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
 
-		public override void Deserialize( GenericReader reader )
-		{
-			base.Deserialize( reader );
+            writer.Write((int)0); // version
+        }
 
-			int version = reader.ReadInt();
-		}
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+
+            int version = reader.ReadInt();
+        }
     }
 }
-
-
-	/*	public bool Dye( Mobile from, DyeTub sender )
-		{
-			if ( Deleted )
-				return false;
-
-			Hue = sender.DyedHue;
-
-			return true;
-		}
-
-		public override void OnDoubleClick( Mobile from )
-		{
-			if ( IsChildOf( from.Backpack ) )
-			{
-				from.SendLocalizedMessage( 502655 ); // What spinning wheel do you wish to spin this on?
-				from.Target = new PickWheelTarget( this );
-			}
-			else
-			{
-				from.SendLocalizedMessage( 1042001 ); // That must be in your pack for you to use it.
-			}
-		}
-
-		public static void OnSpun( ISpinningWheel wheel, Mobile from, int hue )
-		{
-			Item item = new DarkYarn( 3 );
-			item.Hue = hue;
-
-			from.AddToBackpack( item );
-			from.SendLocalizedMessage( 1010576 ); // You put the balls of yarn in your backpack.
-		}
-
-		private class PickWheelTarget : Target
-		{
-            private BritannianWool m_Wool;
-
-            public PickWheelTarget(BritannianWool wool)
-                : base(3, false, TargetFlags.None)
-			{
-				m_Wool = wool;
-			}
-
-			protected override void OnTarget( Mobile from, object targeted )
-			{
-				if ( m_Wool.Deleted )
-					return;
-
-				ISpinningWheel wheel = targeted as ISpinningWheel;
-
-				if ( wheel == null && targeted is AddonComponent )
-					wheel = ((AddonComponent)targeted).Addon as ISpinningWheel;
-
-				if ( wheel is Item )
-				{
-					Item item = (Item)wheel;
-
-					if ( !m_Wool.IsChildOf( from.Backpack ) )
-					{
-						from.SendLocalizedMessage( 1042001 ); // That must be in your pack for you to use it.
-					}
-					else if ( wheel.Spinning )
-					{
-						from.SendLocalizedMessage( 502656 ); // That spinning wheel is being used.
-					}
-					else
-					{
-						m_Wool.Consume();
-						wheel.BeginSpin( new SpinCallback( Wool.OnSpun ), from, m_Wool.Hue );
-					}
-				}
-				else
-				{
-					from.SendLocalizedMessage( 502658 ); // Use that on a spinning wheel.
-				}  
-			}
-		}
-	}
-}  */
