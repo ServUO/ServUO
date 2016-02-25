027b93459f977bd8df4241f9b5585abc20e2c44d
Norman B. Lancaster <qbradq@gmail.com>
2016-02-25 03:06:35 -0600

    Fixes #456

--- Scripts/Mobiles/Vendors/BaseVendor.cs
+++ Scripts/Mobiles/Vendors/BaseVendor.cs
@@ -1327,7 +1327,10 @@ namespace Server.Mobiles
 				}
 			}
 
-			if (!bought && totalCost >= 2000)
+			if (!bought &&
+                (totalCost >= 2000 ||
+                AccountGold.Enabled)
+            )
 			{
 				if (Banker.Withdraw(buyer, totalCost))
 				{
