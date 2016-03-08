def619798d01e77b1a09483c33b91d2858b4d276
Norman B. Lancaster <qbradq@gmail.com>
2016-03-08 15:46:16 -0600

    Fixed swapped Royal and regular forges

--- Scripts/Items/Skill Items/Imbuing/SoulForge.cs
+++ Scripts/Items/Skill Items/Imbuing/SoulForge.cs
@@ -10,29 +10,28 @@ namespace Server.Items
 		[Constructable]
 		public SoulForge()
 		{
-			AddComponent( new AddonComponent( 0x4263 ), 0, 0, 0 );
-			AddComponent( new AddonComponent( 0x4264 ), +1, 0, 0 );
-			AddComponent( new AddonComponent( 0x4265 ), +2, 0, 0 );
-			AddComponent( new AddonComponent( 0x4266 ), +3, 0, 0 );
+            AddComponent(new AddonComponent(0x4277), 0, 0, 0);
+            AddComponent(new AddonComponent(0x4278), +1, 0, 0);
+            AddComponent(new AddonComponent(0x4279), +2, 0, 0);
+            AddComponent(new AddonComponent(0x427A), +3, 0, 0);
 
-			AddComponent( new AddonComponent( 0x4267 ), 0, +1, 0 );
-			AddComponent( new AddonComponent( 0x4268 ), +1, +1, 0 );
-			AddComponent( new AddonComponent( 0x4269 ), +2, +1, 0 );
-			AddComponent( new AddonComponent( 0x426A ), +3, +1, 0 );
+            AddComponent(new AddonComponent(0x427B), 0, +1, 0);
+            AddComponent(new AddonComponent(0x427C), +1, +1, 0);
+            AddComponent(new AddonComponent(0x427D), +2, +1, 0);
+            AddComponent(new AddonComponent(0x427E), +3, +1, 0);
 
-			AddComponent( new AddonComponent( 0x426B ), 0, +2, 0 );
-			AddComponent( new AddonComponent( 0x426C ), +1, +2, 0 );
-			AddComponent( new AddonComponent( 0x426D ), +2, +2, 0 );
-			AddComponent( new AddonComponent( 0x426E ), +3, +2, 0 );
+            AddComponent(new AddonComponent(0x427F), 0, +2, 0);
+            AddComponent(new AddonComponent(0x4280), +1, +2, 0);
+            AddComponent(new AddonComponent(0x4281), +2, +2, 0);
+            AddComponent(new AddonComponent(0x4282), +3, +2, 0);
 
-			AddComponent( new AddonComponent( 0x426F ), 0, +3, 0 );
-			AddComponent( new AddonComponent( 0x4270 ), +1, +3, 0 );
-			AddComponent( new AddonComponent( 0x4271 ), +2, +3, 0 );
-			AddComponent( new AddonComponent( 0x4272 ), +3, +3, 0 );
+            AddComponent(new AddonComponent(0x4283), 0, +3, 0);
+            AddComponent(new AddonComponent(0x4284), +1, +3, 0);
+            AddComponent(new AddonComponent(0x4285), +2, +3, 0);
+            AddComponent(new AddonComponent(0x4286), +3, +3, 0);
+        }
 
-		}
-
-		public SoulForge( Serial serial ) : base( serial )
+        public SoulForge( Serial serial ) : base( serial )
 		{
 		}
 
