932d8ea316ea6cf7e43938fc5de61509e8d24425
nightshark <jackward84@gmail.com>
2016-04-07 19:27:22 +1200

    Fixes "get to trammel" exploit for shards without trammel.

--- Scripts/Services/Help/StuckMenu.cs
+++ Scripts/Services/Help/StuckMenu.cs
@@ -1,6 +1,9 @@
 using System;
 using Server.Gumps;
 using Server.Network;
+using System.Collections.Generic;
+using System.Linq;
+using Server.Mobiles;
 
 namespace Server.Menus.Questions
 {
@@ -262,6 +265,17 @@ namespace Server.Menus.Questions
                 this.m_End = DateTime.UtcNow + delay;
             }
 
+            private void MovePetsOfLoggedCharacter(Point3D dest, Map destMap)
+            {
+                Map fromMap = m_Mobile.LogoutMap;
+                Point3D fromLoc = m_Mobile.LogoutLocation;
+                fromMap.GetMobilesInRange(fromLoc, 3);
+                var move = fromMap.GetMobilesInRange(fromLoc, 3).Where(m => m is BaseCreature).Cast<BaseCreature>()
+                    .Where(pet => pet.Controlled && pet.ControlMaster == m_Mobile && pet.ControlOrder == OrderType.Guard || pet.ControlOrder == OrderType.Follow || pet.ControlOrder == OrderType.Come).ToList();
+
+                move.ForEach(x=>x.MoveToWorld(dest,destMap));
+            }
+
             protected override void OnTick()
             {
                 if (DateTime.UtcNow < this.m_End)
@@ -287,11 +301,24 @@ namespace Server.Menus.Questions
                         destMap = Map.Trammel;
                     else if (this.m_Mobile.Map == Map.Felucca)
                         destMap = Map.Felucca;
+                    else if (m_Mobile.Map == Map.Internal)
+                        destMap = m_Mobile.LogoutMap == Map.Felucca ? Map.Felucca : Map.Trammel;
                     else
                         destMap = this.m_Mobile.Kills >= 5 ? Map.Felucca : Map.Trammel;
 
-                    Mobiles.BaseCreature.TeleportPets(this.m_Mobile, dest, destMap);
-                    this.m_Mobile.MoveToWorld(dest, destMap);
+                    if (m_Mobile.Map != Map.Internal)
+                    {
+                        Mobiles.BaseCreature.TeleportPets(this.m_Mobile, dest, destMap);
+                        m_Mobile.MoveToWorld(dest, destMap);
+                    }
+                    else
+                    {
+                        // for shards without auto stabling
+                        MovePetsOfLoggedCharacter(dest, destMap);
+                        
+                        m_Mobile.LogoutLocation = dest;
+                        m_Mobile.LogoutMap = destMap;
+                    }
                 }
             }
         }
