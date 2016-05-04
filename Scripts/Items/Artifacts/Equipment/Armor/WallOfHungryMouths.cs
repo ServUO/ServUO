e383e6254b2eaa7d1d18ff1e8ff65a1c68bec1fd
Argalep <argalep@quingis.com>
2016-05-04 16:22:10 +0300

    #754 - Duplicate item

--- Scripts/Items/Artifacts/Equipment/Armor/WallOfHungryMouths.cs
+++ Scripts/Items/Artifacts/Equipment/Armor/WallOfHungryMouths.cs
@@ -10,7 +10,7 @@ namespace Server.Items
             : base(0x1B76)
         {
             this.Weight = 8.0;
-            this.Hue = 1845;
+            this.Hue = 1866;
 
             this.AbsorptionAttributes.EaterEnergy = 20;
             this.AbsorptionAttributes.EaterPoison = 20;
@@ -91,90 +91,6 @@ namespace Server.Items
             get
             {
                 return 23;
-            }
-        }
-        public override void Deserialize(GenericReader reader)
-        {
-            base.Deserialize(reader);
-
-            int version = reader.ReadInt();
-        }
-
-        public override void Serialize(GenericWriter writer)
-        {
-            base.Serialize(writer);
-
-            writer.Write((int)0);//version
-        }
-    }
-
-    public class WallOfHungryMouths : HeaterShield
-    {
-        [Constructable]
-        public WallOfHungryMouths()
-        {
-            this.Name = ("Wall Of Hungry Mouths");
-
-            this.Hue = 1866;
-
-            this.AbsorptionAttributes.EaterEnergy = 10;
-            this.AbsorptionAttributes.EaterPoison = 10;
-            this.AbsorptionAttributes.EaterCold = 10;
-            this.AbsorptionAttributes.EaterFire = 10;
-        }
-
-        public WallOfHungryMouths(Serial serial)
-            : base(serial)
-        {
-        }
-
-        public override int BasePhysicalResistance
-        {
-            get
-            {
-                return 5;
-            }
-        }
-        public override int BaseFireResistance
-        {
-            get
-            {
-                return 1;
-            }
-        }
-        public override int BaseColdResistance
-        {
-            get
-            {
-                return 0;
-            }
-        }
-        public override int BasePoisonResistance
-        {
-            get
-            {
-                return 0;
-            }
-        }
-        public override int BaseEnergyResistance
-        {
-            get
-            {
-                return 0;
-            }
-        }
-        public override int InitMinHits
-        {
-            get
-            {
-                return 255;
-            }
-        }
-        public override int InitMaxHits
-        {
-            get
-            {
-                return 255;
             }
         }
         public override void Deserialize(GenericReader reader)
