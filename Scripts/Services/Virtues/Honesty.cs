25c376e5e406ea6f29a16b7fdc92afd94c6dd26a
Darragh Murphy <irishdog22@gmail.com>
2016-05-13 01:48:51 +0100

    Item generation improved. Checks if item has been picked up, if so will remove from list and generate a new item. Check at first save if their are HonestyItems already in the world, if so add to list preventing duplicate spawning. I believe item generation to be complete for initial introduction into master branch.

--- Scripts/Services/Virtues/Honesty.cs
+++ Scripts/Services/Virtues/Honesty.cs
@@ -1,62 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Server.Items;
 
 namespace Server.Services.Virtues
 {
-	class Honesty
-	{
-        private static List<Item> HonestyItems = new List<Item>();
+    class Honesty
+    {
+        private static List<Item> _HonestyItems = new List<Item>();
 
-		public static void Initialize()
-		{
-			VirtueGump.Register(106, OnVirtueUsed);
+        public static void Initialize()
+        {
+            VirtueGump.Register(106, OnVirtueUsed);
             EventSink.AfterWorldSave += EventSinkAfterWorldSave;
         }
 
-	    public static void OnVirtueUsed(Mobile from)
-		{
-			from.SendLocalizedMessage(1053001); // This virtue is not activated through the virtue menu.
-		}
+
+        public static void OnVirtueUsed(Mobile from)
+        {
+            from.SendLocalizedMessage(1053001); // This virtue is not activated through the virtue menu.
+        }
 
         private static void EventSinkAfterWorldSave(AfterWorldSaveEventArgs worldSaveEventArgs)
         {
 
             //Heavy operation, let's use a seperate thread.
             Task.Factory.StartNew(GenerateHonestyItems);
-            
+
         }
 
-	    private static void GenerateHonestyItems()
-	    {
-            for (int i = 0; i < 50; i++)
+        private static void GenerateHonestyItems()
+        {
+            if (_HonestyItems.Count == 0)
+            {
+                var list =
+                    World.Items.Values.Where(
+                        m =>
+                            m.HonestyItem).ToList();
+            
+
+            foreach (Item i in list)
+                {
+                    _HonestyItems.Add(i);
+                }
+            }
+
+            foreach (Item i in _HonestyItems)
+            {
+                if (ItemFlags.GetTaken(i))
+                {
+                    _HonestyItems.Remove(i);
+                }
+            }
+
+            while (_HonestyItems.Count < 500)
             {
                 var toSpawn = Loot.RandomArmorOrShieldOrWeapon();
                 ItemFlags.SetTaken(toSpawn, false);
                 toSpawn.HonestyItem = true;
-                HonestyItems.Add(toSpawn);
+                _HonestyItems.Add(toSpawn);
 
                 PlaceItemOnWorld(toSpawn);
             }
         }
 
-	    private static void PlaceItemOnWorld(Item item)
-	    {
-	        Rectangle2D rect = new Rectangle2D(0, 0, 5119, 4095);
-            int x = 0;
-            int y = 0;
+        private static void PlaceItemOnWorld(Item item)
+        {
+            Rectangle2D rect = new Rectangle2D(0, 0, 5119, 4095);
 
             var placeCoords = new Point3D();
 
-	        Map map = Utility.RandomBool() ? Map.Trammel : Map.Felucca;
+            Map map = Utility.RandomBool() ? Map.Trammel : Map.Felucca;
 
             while (true)
             {
-                x = Utility.Random(rect.X, rect.Width);
-                y = Utility.Random(rect.Y, rect.Height);
+                var x = Utility.Random(rect.X, rect.Width);
+                var y = Utility.Random(rect.Y, rect.Height);
 
                 if (!TreasureMap.ValidateLocation(x, y, map)) continue;
                 placeCoords.X = x;
