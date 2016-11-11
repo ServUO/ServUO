75d6d901a091f07b16fab6a9019540f700a29b88
Dexter <kevin_bourne10@yahoo.com>
2016-11-11 11:44:11 -0700

    Added Functionality to RefinementAmalgamator

--- Scripts/Services/CleanUpBritannia/Items/RefinementAmalgamator.cs
+++ Scripts/Services/CleanUpBritannia/Items/RefinementAmalgamator.cs
@@ -35,25 +35,68 @@ namespace Server.Items
             }
             else
             {
-                from.SendLocalizedMessage(1060640); // Target the refinement you wish to combine.
+                from.SendLocalizedMessage(1154351); // Target the refinement you wish to combine.
                 from.Target = new InternalTarget(from);
             }
         }
 
         private class InternalTarget : Target
         {
-            private readonly Mobile m_Mobile;
+            private Mobile m_Mobile;
+            private RefinementComponent m_First;
 
-            public InternalTarget(Mobile m)
+            public InternalTarget(Mobile m, RefinementComponent first = null)
                 : base(-1, true, TargetFlags.None)
             {
-                this.m_Mobile = m;
+                m_Mobile = m;
+                m_First = first;
             }
 
             protected override void OnTarget(Mobile from, object targeted)
             {
-                //from.SendLocalizedMessage(1154456); // You must first select a refinement to combine. 
-                //from.SendLocalizedMessage(1154457); // This is not a refinement.
+                Item item = targeted as Item;
+
+                if (item == null)
+                    return;
+
+                if(!item.IsChildOf(from.Backpack))
+                    from.SendLocalizedMessage(1060640); // The item must be in your backpack to use it.
+                else if (targeted is RefinementComponent)
+                {
+                    RefinementComponent comp = (RefinementComponent)targeted;
+
+                    if (m_First == null)
+                    {
+                        if (comp.ModType == ModType.Invulnerability)
+                        {
+                            from.SendLocalizedMessage(1154353); // You can't upgrade this refinement.
+                        }
+                        else
+                        {
+                            from.SendLocalizedMessage(1154351); // Target the refinement you wish to combine.
+                            from.Target = new InternalTarget(from, comp);
+                        }
+                    }
+                    else
+                    {
+                        if (m_First.RefinementType != comp.RefinementType 
+                            || m_First.CraftType != comp.CraftType 
+                            || m_First.SubCraftType != comp.SubCraftType
+                            || m_First.ModType != comp.ModType)
+                        {
+                            from.SendLocalizedMessage(1154354); // This refinement does not match the type currently being combined.
+                        }
+                        else
+                        {
+                            comp.Delete();
+                            m_First.ModType++;
+
+                            from.SendLocalizedMessage(1154352); // You've completed the amalgamation and received an upgraded version of your refinement.
+                        }
+                    }
+                }
+                else
+                    from.SendLocalizedMessage(1154457); // This is not a refinement.
             }
         }
 
