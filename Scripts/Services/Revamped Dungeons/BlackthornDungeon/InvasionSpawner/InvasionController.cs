4459a8822180398375e11467f62ade46ce6e000a
Argalep <argalep@quingis.com>
2016-12-19 12:47:44 +0200

    Invasion Trinsic Spawn Bug Fix

--- Scripts/Services/Revamped Dungeons/BlackthornDungeon/InvasionSpawner/InvasionController.cs
+++ Scripts/Services/Revamped Dungeons/BlackthornDungeon/InvasionSpawner/InvasionController.cs
@@ -587,7 +587,7 @@ namespace Server.Engines.Blackthorn
                 new Rectangle2D[] 
                 {
                     new Rectangle2D(6356, 2371, 10, 10),
-                    new Rectangle2D(5354, 2344, 5, 10),
+                    new Rectangle2D(6354, 2344, 5, 10),
                     new Rectangle2D(6366, 2344, 5, 7),
                     new Rectangle2D(6386, 2355, 8, 8),
                 }, 
