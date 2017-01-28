212ff09ab9f8c7602c54e1b21c09099392083058
maicomstoppa <maicom.stoppa@gmail.com>
2017-01-28 21:02:21 -0200

    Revert "Revert "ec-toolbar-mysticismfix""
    This reverts commit 5413bb3956384381741a9075780b95a877e86f88.

--- Scripts/Spells/Base/Spell.cs
+++ Scripts/Spells/Base/Spell.cs
@@ -827,7 +827,9 @@ namespace Server.Spells
             MethodInfo spellTargetMethod = null;
             if (spellType != null && (spellTargetMethod = spellType.GetMethod("Target")) != null) {
 
-            } else {
+            } else if(spellType != null && (spellTargetMethod = spellType.GetMethod("OnTarget")) != null) {
+
+            }else {
                 OnCast();
                 return;
             }
@@ -836,7 +838,7 @@ namespace Server.Spells
             object[] targetArgs = null;
             if (spellTargetParams != null && spellTargetParams.Length > 0) {
 
-                if ((InstantTarget is Mobile) && (spellTargetParams[0].ParameterType == typeof(Server.Mobile) || spellTargetParams[0].ParameterType == typeof(Server.IDamageable))) {
+                if ((InstantTarget is Mobile) && (spellTargetParams[0].ParameterType == typeof(Server.Mobile) || spellTargetParams[0].ParameterType == typeof(Server.IDamageable) || spellTargetParams[0].ParameterType == typeof(Object))) {
                     targetArgs = new object[1];
                     targetArgs[0] = InstantTarget;
                 } else if ((InstantTarget is Mobile) && (spellTargetParams[0].ParameterType == typeof(Server.IPoint3D))){
