2c093d5667bd290189a60494538252d7b7579531
Argalep <argalep@quingis.com>
2017-02-05 15:54:49 +0200

    Secrets of the Soulforge Reward Item Fix.
    http://www.uoguide.com/Secrets_of_the_Soulforge
    
    * only  A Wondrous or An Exalted Scroll of Imbuing (105 or 110 Skill)

--- Scripts/Items/Containers/ScrollBox3.cs
+++ Scripts/Items/Containers/ScrollBox3.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Server.Items
 {
-    public class ScrollBox3 : WoodenBox
+    public class ScrollBox3 : WoodenBox // Secrets of the Soulforge Reward Item
     {
         [Constructable]	
         public ScrollBox3()
@@ -11,56 +11,13 @@ namespace Server.Items
             this.Movable = true;
             this.Hue = 1159;
 
-            DropItem(new PowerScroll(SkillName.Imbuing, 115.0));
-
-            if (0.05 >= Utility.RandomDouble())
+            if (Utility.RandomBool())
             {
-                double runictype = Utility.RandomDouble();
-                CraftResource res;
-                int charges;
-
-                if (runictype <= .25)
-                {
-                    res = CraftResource.DullCopper;
-                    charges = 50;
-                }
-                else if (runictype <= .40)
-                {
-                    res = CraftResource.ShadowIron;
-                    charges = 45;
-                }
-                else if (runictype <= .55)
-                {
-                    res = CraftResource.Copper;
-                    charges = 40;
-                }
-                else if (runictype <= .65)
-                {
-                    res = CraftResource.Bronze;
-                    charges = 35;
-                }
-                else if (runictype <= .75)
-                {
-                    res = CraftResource.Gold;
-                    charges = 30;
-                }
-                else if (runictype <= .85)
-                {
-                    res = CraftResource.Agapite;
-                    charges = 25;
-                }
-                else if (runictype <= .98)
-                {
-                    res = CraftResource.Verite;
-                    charges = 20;
-                }
-                else
-                {
-                    res = CraftResource.Valorite;
-                    charges = 15;
-                }
-
-                DropItem(new RunicMalletAndChisel(res, charges));
+                this.DropItem(new PowerScroll(SkillName.Imbuing, 110.0));
+            }
+            else
+            {
+                this.DropItem(new PowerScroll(SkillName.Imbuing, 105.0));
             }
         }
 
