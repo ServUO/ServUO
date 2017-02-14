171460613ee8b8916226450b467fb83c241321e1
Dexter <kevin_bourne10@yahoo.com>
2017-02-14 10:49:40 -0700

    Update CuffsOfTheArchmage.cs

--- Scripts/Items/Artifacts/Equipment/Armor/CuffsOfTheArchmage.cs
+++ Scripts/Items/Artifacts/Equipment/Armor/CuffsOfTheArchmage.cs
@@ -34,7 +34,7 @@ namespace Server.Items
         {
             base.Serialize(writer);
 
-            writer.WriteEncodedInt(0); // version
+            writer.Write(0); // version
         }
 
         public override void Deserialize(GenericReader reader)
