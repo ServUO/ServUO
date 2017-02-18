3c9074e8dd47bd588723572c11da5e485d794a7c
Xeroxxx <fallenwarrior@gmx.de>
2017-02-18 17:14:32 +0100

    Update ItemBounds.cs
    Loading correct Bounds.bin

--- Server/ItemBounds.cs
+++ Server/ItemBounds.cs
@@ -16,36 +16,53 @@ namespace Server
 		private static readonly Rectangle2D[] m_Bounds;
 
 		public static Rectangle2D[] Table { get { return m_Bounds; } }
+		
+		private static string m_Path = "Cache/Bounds.bin";
+		
+		private static string m_LegacyPath = "Data/Binary/Bounds.bin";
 
 		static ItemBounds()
 		{
 			m_Bounds = new Rectangle2D[TileData.ItemTable.Length];
-
-			if (File.Exists("Data/Binary/Bounds.bin"))
+			
+			if (File.Exists(m_Path))
 			{
-				using (FileStream fs = new FileStream("Data/Binary/Bounds.bin", FileMode.Open, FileAccess.Read, FileShare.Read))
+				ReadItemBounds(m_Path);
+				if (Core.Debug)
+						Console.WriteLine("Using generated Bounds.bin");
+			}
+			else if (!File.Exists(m_Path))
+			{
+				ReadItemBounds(m_LegacyPath);
+				if (Core.Debug)
+						Console.WriteLine("Using Generic Bounds.bin");
+			}
+			else
+			{
+				Console.WriteLine("Warning: Bounds.bin does not exist");
+			}
+		}
+			
+			
+		private static void ReadItemBounds(string Path)
+		{
+				using (FileStream fs = new FileStream(Path, FileMode.Open, FileAccess.Read, FileShare.Read))
 				{
 					BinaryReader bin = new BinaryReader(fs);
 
 					int count = Math.Min(m_Bounds.Length, (int)(fs.Length / 8));
-
+					
 					for (int i = 0; i < count; ++i)
 					{
 						int xMin = bin.ReadInt16();
 						int yMin = bin.ReadInt16();
 						int xMax = bin.ReadInt16();
 						int yMax = bin.ReadInt16();
-
+										
 						m_Bounds[i].Set(xMin, yMin, (xMax - xMin) + 1, (yMax - yMin) + 1);
 					}
-
 					bin.Close();
 				}
-			}
-			else
-			{
-				Console.WriteLine("Warning: Data/Binary/Bounds.bin does not exist");
-			}
 		}
 	}
 }
