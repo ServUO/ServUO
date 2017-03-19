ae383104297ebe9c718140add640d1fb1c9bc73e
Argalep <argalep@quingis.com>
2017-03-19 23:27:42 +0300

    SorcerersRewardChest respawn fix.
    Note: Add it again

--- Scripts/Services/ExploringTheDeep/Items/SorcerersRewardChest.cs
+++ Scripts/Services/ExploringTheDeep/Items/SorcerersRewardChest.cs
@@ -5,18 +5,121 @@ using Server.Network;
 
 namespace Server.Items
 {
-    [FlipableAttribute(0xe41, 0xe40)]
-    public class SorcerersRewardChest : BaseTreasureChestMod
+    public class SorcerersRewardChest : LockableContainer
     {
         public override int LabelNumber { get { return 1023712; } } // strong box
+        private short m_MaxSpawnTime = 10;
+        private short m_MinSpawnTime = 5;
+        private ResetTimer m_ResetTimer;
+
+        [CommandProperty(AccessLevel.GameMaster)]
+        public short MaxSpawnTime
+        {
+            get { return this.m_MaxSpawnTime; }
+            set { this.m_MaxSpawnTime = value; }
+        }
+
+        [CommandProperty(AccessLevel.GameMaster)]
+        public short MinSpawnTime
+        {
+            get { return this.m_MinSpawnTime; }
+            set { this.m_MinSpawnTime = value; }
+        }
+
+        [CommandProperty(AccessLevel.GameMaster)]
+        public override bool Locked
+        {
+            get { return base.Locked; }
+            set
+            {
+                if (base.Locked != value)
+                {
+                    base.Locked = value;
+
+                    if (!value)
+                        this.StartResetTimer();
+                }
+            }
+        }
+        public override bool IsDecoContainer { get { return false; } }
 
         [Constructable]
         public SorcerersRewardChest() : base(0x9AA)
         {
             this.Locked = true;
+            this.Movable = false;
             this.Hue = 1912;
             TrapType = TrapType.MagicTrap;
             TrapPower = 4 * Utility.Random(1, 25);
+            this.GenerateTreasure();
+        }
+
+        public override void OnDoubleClick(Mobile from)
+        {
+            if (this.Locked)
+            {
+                from.PublicOverheadMessage(MessageType.Regular, 0x3B2, 1154226); // *It's an unassuming strong box. You examine the lock more closely and determine there is no way to pick it. You'll need to find a key.*
+                return;
+            }
+
+            base.OnDoubleClick(from);
+        }
+
+        public SorcerersRewardChest(Serial serial) : base(serial)
+        {
+        }
+
+        public override void OnTelekinesis(Mobile from)
+        {
+            from.SendLocalizedMessage(501857); // This spell won't work on that!
+            return;
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+            writer.Write((int)0); // version
+
+            writer.Write(this.m_MinSpawnTime);
+            writer.Write(this.m_MaxSpawnTime);
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+            int version = reader.ReadInt();
+
+            this.m_MinSpawnTime = reader.ReadShort();
+            this.m_MaxSpawnTime = reader.ReadShort();
+
+            if (!this.Locked)
+                this.StartResetTimer();
+        }
+
+        public void ClearContents()
+        {
+            for (int i = this.Items.Count - 1; i >= 0; --i)
+            {
+                if (i < this.Items.Count)
+                    this.Items[i].Delete();
+            }
+        }
+
+        public void Reset()
+        {
+            if (this.m_ResetTimer != null)
+            {
+                if (this.m_ResetTimer.Running)
+                    this.m_ResetTimer.Stop();
+            }
+
+            this.Locked = true;
+            this.ClearContents();
+            this.GenerateTreasure();
+        }
+
+        protected virtual void GenerateTreasure()
+        {
             DropItem(new Gold(200, 400));
             DropItem(new BlankScroll(Utility.Random(1, 4)));
 
@@ -40,7 +143,7 @@ namespace Server.Items
 
             for (int i = Utility.Random(1, 4); i > 1; i--)
                 DropItem(Loot.RandomWand());
-            
+
 
             for (int i = Utility.Random(3) + 1; i > 0; i--) // random 1 to 3
             {
@@ -53,31 +156,30 @@ namespace Server.Items
             }
         }
 
-        public override void OnDoubleClick(Mobile from)
+        private void StartResetTimer()
         {
-            if (this.Locked)
+            if (this.m_ResetTimer == null)
+                this.m_ResetTimer = new ResetTimer(this);
+            else
+                this.m_ResetTimer.Delay = TimeSpan.FromMinutes(Utility.Random(this.m_MinSpawnTime, this.m_MaxSpawnTime));
+
+            this.m_ResetTimer.Start();
+        }
+
+        private class ResetTimer : Timer
+        {
+            private readonly SorcerersRewardChest m_Chest;
+            public ResetTimer(SorcerersRewardChest chest)
+                : base(TimeSpan.FromMinutes(Utility.Random(chest.MinSpawnTime, chest.MaxSpawnTime)))
             {
-                from.PublicOverheadMessage(MessageType.Regular, 0x3B2, 1154226); // *It's an unassuming strong box. You examine the lock more closely and determine there is no way to pick it. You'll need to find a key.*
-                return;
+                this.m_Chest = chest;
+                this.Priority = TimerPriority.OneMinute;
             }
 
-            base.OnDoubleClick(from);
-        }
-
-        public SorcerersRewardChest(Serial serial) : base(serial)
-        {
-        }
-
-        public override void Serialize(GenericWriter writer)
-        {
-            base.Serialize(writer);
-            writer.Write((int)0); // version 
-        }
-
-        public override void Deserialize(GenericReader reader)
-        {
-            base.Deserialize(reader);
-            int version = reader.ReadInt();
+            protected override void OnTick()
+            {
+                this.m_Chest.Reset();
+            }
         }
     }
 
