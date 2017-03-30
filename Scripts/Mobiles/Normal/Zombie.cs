647845773bead500a5668799f97b337ed277fa33
GriffonSpade <GriffonSpade@hotmail.com>
2017-03-30 06:17:47 -0500

    Zombie.cs: OppositionList

--- Scripts/Mobiles/Normal/Zombie.cs
+++ Scripts/Mobiles/Normal/Zombie.cs
@@ -10,64 +10,64 @@ namespace Server.Mobiles
         public Zombie()
             : base(AIType.AI_Melee, FightMode.Closest, 10, 1, 0.2, 0.4)
         {
-            this.Name = "a zombie";
-            this.Body = 3;
-            this.BaseSoundID = 471;
+            Name = "a zombie";
+            Body = 3;
+            BaseSoundID = 471;
 
-            this.SetStr(46, 70);
-            this.SetDex(31, 50);
-            this.SetInt(26, 40);
+            SetStr(46, 70);
+            SetDex(31, 50);
+            SetInt(26, 40);
 
-            this.SetHits(28, 42);
+            SetHits(28, 42);
 
-            this.SetDamage(3, 7);
+            SetDamage(3, 7);
 
-            this.SetDamageType(ResistanceType.Physical, 100);
+            SetDamageType(ResistanceType.Physical, 100);
 
-            this.SetResistance(ResistanceType.Physical, 15, 20);
-            this.SetResistance(ResistanceType.Cold, 20, 30);
-            this.SetResistance(ResistanceType.Poison, 5, 10);
+            SetResistance(ResistanceType.Physical, 15, 20);
+            SetResistance(ResistanceType.Cold, 20, 30);
+            SetResistance(ResistanceType.Poison, 5, 10);
 
-            this.SetSkill(SkillName.MagicResist, 15.1, 40.0);
-            this.SetSkill(SkillName.Tactics, 35.1, 50.0);
-            this.SetSkill(SkillName.Wrestling, 35.1, 50.0);
+            SetSkill(SkillName.MagicResist, 15.1, 40.0);
+            SetSkill(SkillName.Tactics, 35.1, 50.0);
+            SetSkill(SkillName.Wrestling, 35.1, 50.0);
 
-            this.Fame = 600;
-            this.Karma = -600;
+            Fame = 600;
+            Karma = -600;
 
-            this.VirtualArmor = 18;
-			
+            VirtualArmor = 18;
+
             switch ( Utility.Random(10))
             {
                 case 0:
-                    this.PackItem(new LeftArm());
+                    PackItem(new LeftArm());
                     break;
                 case 1:
-                    this.PackItem(new RightArm());
+                    PackItem(new RightArm());
                     break;
                 case 2:
-                    this.PackItem(new Torso());
+                    PackItem(new Torso());
                     break;
                 case 3:
-                    this.PackItem(new Bone());
+                    PackItem(new Bone());
                     break;
                 case 4:
-                    this.PackItem(new RibCage());
+                    PackItem(new RibCage());
                     break;
                 case 5:
-                    this.PackItem(new RibCage());
+                    PackItem(new RibCage());
                     break;
                 case 6:
-                    this.PackItem(new BonePile());
+                    PackItem(new BonePile());
                     break;
                 case 7:
-                    this.PackItem(new BonePile());
+                    PackItem(new BonePile());
                     break;
                 case 8:
-                    this.PackItem(new BonePile());
+                    PackItem(new BonePile());
                     break;
                 case 9:
-                    this.PackItem(new BonePile());
+                    PackItem(new BonePile());
                     break;
             }
         }
@@ -77,30 +77,13 @@ namespace Server.Mobiles
         {
         }
 
-        public override bool BleedImmune
-        {
-            get
-            {
-                return true;
-            }
-        }
-        public override Poison PoisonImmune
-        {
-            get
-            {
-                return Poison.Regular;
-            }
-        }
-        public override OppositionGroup OppositionGroup
-        {
-            get
-            {
-                return OppositionGroup.FeyAndUndead;
-            }
-        }
+        public override bool BleedImmune{ get{ return true; } }
+        public override OppositionType OppositionList{ get{ return OppositionType.Undead; } }
+        public override Poison PoisonImmune{ get{ return Poison.Regular; } }
+
         public override void GenerateLoot()
         {
-            this.AddLoot(LootPack.Meager);
+            AddLoot(LootPack.Meager);
         }
 
         public override void Serialize(GenericWriter writer)
