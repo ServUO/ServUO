bfde5c7c1df5a96d3fc146cb08a25541bdf464cc
GriffonSpade <GriffonSpade@hotmail.com>
2017-03-30 06:13:39 -0500

    TerathanAvenger.cs: OppositionList

--- Scripts/Mobiles/Normal/TerathanAvenger.cs
+++ Scripts/Mobiles/Normal/TerathanAvenger.cs
@@ -9,39 +9,39 @@ namespace Server.Mobiles
         public TerathanAvenger()
             : base(AIType.AI_Mage, FightMode.Closest, 10, 1, 0.2, 0.4)
         {
-            this.Name = "a terathan avenger";
-            this.Body = 152;
-            this.BaseSoundID = 0x24D;
+            Name = "a terathan avenger";
+            Body = 152;
+            BaseSoundID = 0x24D;
 
-            this.SetStr(467, 645);
-            this.SetDex(77, 95);
-            this.SetInt(126, 150);
+            SetStr(467, 645);
+            SetDex(77, 95);
+            SetInt(126, 150);
 
-            this.SetHits(296, 372);
-            this.SetMana(46, 70);
+            SetHits(296, 372);
+            SetMana(46, 70);
 
-            this.SetDamage(18, 22);
+            SetDamage(18, 22);
 
-            this.SetDamageType(ResistanceType.Physical, 50);
-            this.SetDamageType(ResistanceType.Poison, 50);
+            SetDamageType(ResistanceType.Physical, 50);
+            SetDamageType(ResistanceType.Poison, 50);
 
-            this.SetResistance(ResistanceType.Physical, 40, 50);
-            this.SetResistance(ResistanceType.Fire, 30, 40);
-            this.SetResistance(ResistanceType.Cold, 35, 45);
-            this.SetResistance(ResistanceType.Poison, 90, 100);
-            this.SetResistance(ResistanceType.Energy, 35, 45);
+            SetResistance(ResistanceType.Physical, 40, 50);
+            SetResistance(ResistanceType.Fire, 30, 40);
+            SetResistance(ResistanceType.Cold, 35, 45);
+            SetResistance(ResistanceType.Poison, 90, 100);
+            SetResistance(ResistanceType.Energy, 35, 45);
 
-            this.SetSkill(SkillName.EvalInt, 70.3, 100.0);
-            this.SetSkill(SkillName.Magery, 70.3, 100.0);
-            this.SetSkill(SkillName.Poisoning, 60.1, 80.0);
-            this.SetSkill(SkillName.MagicResist, 65.1, 80.0);
-            this.SetSkill(SkillName.Tactics, 90.1, 100.0);
-            this.SetSkill(SkillName.Wrestling, 90.1, 100.0);
+            SetSkill(SkillName.EvalInt, 70.3, 100.0);
+            SetSkill(SkillName.Magery, 70.3, 100.0);
+            SetSkill(SkillName.Poisoning, 60.1, 80.0);
+            SetSkill(SkillName.MagicResist, 65.1, 80.0);
+            SetSkill(SkillName.Tactics, 90.1, 100.0);
+            SetSkill(SkillName.Wrestling, 90.1, 100.0);
 
-            this.Fame = 15000;
-            this.Karma = -15000;
+            Fame = 15000;
+            Karma = -15000;
 
-            this.VirtualArmor = 50;
+            VirtualArmor = 50;
         }
 
         public TerathanAvenger(Serial serial)
@@ -49,44 +49,15 @@ namespace Server.Mobiles
         {
         }
 
-        public override Poison PoisonImmune
-        {
-            get
-            {
-                return Poison.Deadly;
-            }
-        }
-        public override Poison HitPoison
-        {
-            get
-            {
-                return Poison.Deadly;
-            }
-        }
-        public override int TreasureMapLevel
-        {
-            get
-            {
-                return 3;
-            }
-        }
-        public override int Meat
-        {
-            get
-            {
-                return 2;
-            }
-        }
-        public override OppositionGroup OppositionGroup
-        {
-            get
-            {
-                return OppositionGroup.TerathansAndOphidians;
-            }
-        }
+        public override int Meat{ get{ return 2; } }
+        public override int TreasureMapLevel { get { return 3; } }
+        public override OppositionType OppositionList{ get{ return OppositionType.Terathan; } }
+        public override Poison HitPoison{ get{ return Poison.Deadly; } }
+        public override Poison PoisonImmune{ get{ return Poison.Deadly; } }
+
         public override void GenerateLoot()
         {
-            this.AddLoot(LootPack.Rich, 2);
+            AddLoot(LootPack.Rich, 2);
         }
 
         public override void Serialize(GenericWriter writer)
@@ -100,8 +71,8 @@ namespace Server.Mobiles
             base.Deserialize(reader);
             int version = reader.ReadInt();
 
-            if (this.BaseSoundID == 263)
-                this.BaseSoundID = 0x24D;
+            if (BaseSoundID == 263)
+                BaseSoundID = 0x24D;
         }
     }
 }
