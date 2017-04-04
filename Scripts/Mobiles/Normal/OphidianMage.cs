eee619f0ccfa35f610be403003790ab75c902a78
GriffonSpade <GriffonSpade@hotmail.com>
2017-04-03 22:39:02 -0500

    OphidianMage.cs: Opposition Tribe

--- Scripts/Mobiles/Normal/OphidianMage.cs
+++ Scripts/Mobiles/Normal/OphidianMage.cs
@@ -75,13 +75,9 @@ namespace Server.Mobiles
                 return 2;
             }
         }
-        public override OppositionGroup OppositionGroup
-        {
-            get
-            {
-                return OppositionGroup.TerathansAndOphidians;
-            }
-        }
+
+        public override TribeType Tribe { get { return TribeType.Ophidian; } }
+
         public override void GenerateLoot()
         {
             this.AddLoot(LootPack.Average);
