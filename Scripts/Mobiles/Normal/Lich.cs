06824c8379104e6f7d02e3798e703eec4cf10504
GriffonSpade <GriffonSpade@hotmail.com>
2017-04-03 22:32:50 -0500

    Lich.cs: Opposition Tribe

--- Scripts/Mobiles/Normal/Lich.cs
+++ Scripts/Mobiles/Normal/Lich.cs
@@ -65,13 +65,8 @@ namespace Server.Mobiles
         {
         }
 
-        public override OppositionGroup OppositionGroup
-        {
-            get
-            {
-                return OppositionGroup.FeyAndUndead;
-            }
-        }
+        public override TribeType Tribe { get { return TribeType.Undead; } }
+
         public override bool CanRummageCorpses
         {
             get
