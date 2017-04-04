6249f1fbd7903405f608a3c8d17c8dbf3d8d8ab9
GriffonSpade <GriffonSpade@hotmail.com>
2017-04-03 22:59:36 -0500

    TerathanMatriarch.cs: Opposition Tribe

--- Scripts/Mobiles/Normal/TerathanMatriarch.cs
+++ Scripts/Mobiles/Normal/TerathanMatriarch.cs
@@ -55,13 +55,9 @@ namespace Server.Mobiles
                 return 4;
             }
         }
-        public override OppositionGroup OppositionGroup
-        {
-            get
-            {
-                return OppositionGroup.TerathansAndOphidians;
-            }
-        }
+
+        public override TribeType Tribe { get { return TribeType.Terathan; } }
+
         public override void GenerateLoot()
         {
             this.AddLoot(LootPack.Rich);
