f5c81d5d6ce669dddaf7bebd3c8c7b0e48a036d1
GriffonSpade <GriffonSpade@hotmail.com>
2017-04-03 22:46:05 -0500

    Orc.cs: Opposition Tribe

--- Scripts/Mobiles/Normal/Orc.cs
+++ Scripts/Mobiles/Normal/Orc.cs
@@ -117,13 +117,9 @@ namespace Server.Mobiles
                 return 1;
             }
         }
-        public override OppositionGroup OppositionGroup
-        {
-            get
-            {
-                return OppositionGroup.SavagesAndOrcs;
-            }
-        }
+
+        public override TribeType Tribe { get { return TribeType.Orc; } }
+
         public override void GenerateLoot()
         {
             this.AddLoot(LootPack.Meager);
