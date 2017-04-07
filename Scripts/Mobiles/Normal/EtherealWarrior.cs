9ba26d53330c9f255b9dd2ac4008832fac69d9fa
GriffonSpade <GriffonSpade@hotmail.com>
2017-04-07 14:28:45 -0500

    EtherealWarrior.cs: Readded OppositionGroup

--- Scripts/Mobiles/Normal/EtherealWarrior.cs
+++ Scripts/Mobiles/Normal/EtherealWarrior.cs
@@ -67,6 +67,13 @@ namespace Server.Mobiles
 
         public override TribeType Tribe { get { return TribeType.Fey; } }
 
+        public override OppositionGroup OppositionGroup
+        {
+            get
+            {
+                return OppositionGroup.FeyAndUndead;
+            }
+        }
         public override int Feathers
         {
             get
