3b139bc9ef4c3bf4009f3586656352cd7d5bf7d2
GriffonSpade <GriffonSpade@hotmail.com>
2017-04-07 14:08:49 -0500

    AncientLich.cs: Readded OppositionGroup

--- Scripts/Mobiles/Normal/AncientLich.cs
+++ Scripts/Mobiles/Normal/AncientLich.cs
@@ -55,6 +55,13 @@ namespace Server.Mobiles
 
         public override TribeType Tribe { get { return TribeType.Undead; } }
 
+        public override OppositionGroup OppositionGroup
+        {
+            get
+            {
+                return OppositionGroup.FeyAndUndead;
+            }
+        }
         public override bool Unprovokable
         {
             get
