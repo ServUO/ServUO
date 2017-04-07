acbaf46791e22d1370a6b5ecfc05ba7b88ccf298
GriffonSpade <GriffonSpade@hotmail.com>
2017-04-07 15:11:57 -0500

    SavageRider.cs: Readded OppositionGroup

--- Scripts/Mobiles/Normal/SavageRider.cs
+++ Scripts/Mobiles/Normal/SavageRider.cs
@@ -75,6 +75,13 @@ namespace Server.Mobiles
 
         public override TribeType Tribe { get { return TribeType.Savage; } }
 
+        public override OppositionGroup OppositionGroup
+        {
+            get
+            {
+                return OppositionGroup.SavagesAndOrcs;
+            }
+        }
         public override void GenerateLoot()
         {
             this.AddLoot(LootPack.Average);
