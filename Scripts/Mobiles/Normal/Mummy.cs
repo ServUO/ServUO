4e5785a866e2c9e465d34325370cfbb11dab1094
GriffonSpade <GriffonSpade@hotmail.com>
2017-04-07 14:40:50 -0500

    Mummy.cs: Readded OppositionGroup

--- Scripts/Mobiles/Normal/Mummy.cs
+++ Scripts/Mobiles/Normal/Mummy.cs
@@ -69,6 +69,13 @@ namespace Server.Mobiles
 
         public override TribeType Tribe { get { return TribeType.Undead; } }
 
+        public override OppositionGroup OppositionGroup
+        {
+            get
+            {
+                return OppositionGroup.FeyAndUndead;
+            }
+        }
         public override void GenerateLoot()
         {
             this.AddLoot(LootPack.Rich);
