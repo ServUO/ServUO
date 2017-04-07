478d6333a850faf6a97ea67acf968541ff23df82
GriffonSpade <GriffonSpade@hotmail.com>
2017-04-07 15:07:07 -0500

    Pixie.cs: Readded OppositionGroup

--- Scripts/Mobiles/Normal/Pixie.cs
+++ Scripts/Mobiles/Normal/Pixie.cs
@@ -81,6 +81,13 @@ namespace Server.Mobiles
 
         public override TribeType Tribe { get { return TribeType.Fey; } }
 
+        public override OppositionGroup OppositionGroup
+        {
+            get
+            {
+                return OppositionGroup.FeyAndUndead;
+            }
+        }
         public override void GenerateLoot()
         {
             this.AddLoot(LootPack.LowScrolls);
