463cba2a6335372480e880ef61d34b2ca85994a9
GriffonSpade <GriffonSpade@hotmail.com>
2017-04-07 15:22:38 -0500

    Unicorn.cs: Readded OppositionGroup

--- Scripts/Mobiles/Normal/Unicorn.cs
+++ Scripts/Mobiles/Normal/Unicorn.cs
@@ -86,6 +86,13 @@ namespace Server.Mobiles
 
         public override TribeType Tribe { get { return TribeType.Fey; } }
 
+        public override OppositionGroup OppositionGroup
+        {
+            get
+            {
+                return OppositionGroup.FeyAndUndead;
+            }
+        }
         public override Poison PoisonImmune
         {
             get
