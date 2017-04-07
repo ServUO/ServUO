989b43006ab6a3e7b38a85ccb3c67c8364c8ba34
GriffonSpade <GriffonSpade@hotmail.com>
2017-04-07 14:23:09 -0500

    Bogle.cs: Readded OppositionGroup

--- Scripts/Mobiles/Normal/Bogle.cs
+++ Scripts/Mobiles/Normal/Bogle.cs
@@ -58,6 +58,13 @@ namespace Server.Mobiles
 
         public override TribeType Tribe { get { return TribeType.Undead; } }
 
+        public override OppositionGroup OppositionGroup
+        {
+            get
+            {
+                return OppositionGroup.FeyAndUndead;
+            }
+        }
         public override void GenerateLoot()
         {
             this.AddLoot(LootPack.Meager);
