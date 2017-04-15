59ef6c8c1e5d07f9bfac42792422211f80d40d39
Dexter <kevin_bourne10@yahoo.com>
2017-04-15 10:25:01 -0700

    Added CheckFillables command to Decorate.
    - Delay added in the event the spawners aren't set up yet.  Vendors must be spawned for this commmand to work properly.
    - Increased search range when setting up fillables

--- Scripts/Items/Containers/FillableContainers.cs
+++ Scripts/Items/Containers/FillableContainers.cs
@@ -1597,7 +1597,7 @@ namespace Server.Items
             Mobile nearest = null;
             FillableContent content = null;
 
-            foreach (Mobile mob in map.GetMobilesInRange(loc, 20))
+            foreach (Mobile mob in map.GetMobilesInRange(loc, 60))
             {
                 if (nearest != null && mob.GetDistanceToSqrt(loc) > nearest.GetDistanceToSqrt(loc) && !(nearest is Cobbler && mob is Provisioner))
                     continue;
