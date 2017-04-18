96dfdf356a5e120c38f9a853c12b57587a964ab9
Argalep <argalep@quingis.com>
2017-04-18 16:54:04 +0300

    *add Crimson Drake, Platinum Drake, Stygian Drake * Fix Gargoyle Shade

---
+++ Scripts/Mobiles/Normal/CrimsonDrake.cs
@@ -0,0 +1,86 @@
+using System;
+
+namespace Server.Mobiles
+{
+    [CorpseName("a crimson drake corpse")]
+    public class CrimsonDrake : BaseCreature
+    {
+        [Constructable]
+        public CrimsonDrake()
+            : base(AIType.AI_Melee, FightMode.Closest, 10, 1, 0.2, 0.4)
+        {
+            this.Name = "Crimson Drake";
+            this.Body = 0x58b;
+            this.Female = true;
+            this.BaseSoundID = 362;
+
+            this.SetStr(400, 430);
+            this.SetDex(133, 152);
+            this.SetInt(101, 140);
+
+            this.SetHits(241, 258);
+
+            this.SetDamage(11, 17);
+
+            this.SetDamageType(ResistanceType.Physical, 100);
+
+            this.SetResistance(ResistanceType.Physical, 30, 50);
+            this.SetResistance(ResistanceType.Fire, 30, 40);
+            this.SetResistance(ResistanceType.Cold, 30, 40);
+            this.SetResistance(ResistanceType.Poison, 40, 50);
+            this.SetResistance(ResistanceType.Energy, 30, 50);
+
+            this.SetSkill(SkillName.MagicResist, 65.1, 80.0);
+            this.SetSkill(SkillName.Tactics, 65.1, 90.0);
+            this.SetSkill(SkillName.Wrestling, 65.1, 80.0);
+            this.SetSkill(SkillName.DetectHidden, 50.0, 60.0);
+            this.SetSkill(SkillName.Focus, 5.0, 10.0);
+
+            this.Fame = 5500;
+            this.Karma = -5500;
+
+            this.VirtualArmor = 46;
+
+            this.Tamable = true;
+            this.ControlSlots = 2;
+            this.MinTameSkill = 85.0;
+
+            this.PackReg(3);
+        }
+
+        public CrimsonDrake(Serial serial)
+            : base(serial)
+        {
+        }
+
+        public override bool ReacquireOnMovement { get { return true; } }
+        public override bool HasBreath { get { return true; } } // fire breath enabled
+        public override int TreasureMapLevel { get { return 2; } }
+        public override int Meat { get { return 10; } }
+        public override int DragonBlood { get { return 8; } }
+        public override int Hides { get { return 22; } }
+        public override HideType HideType { get { return HideType.Horned; } }
+        public override int Scales { get { return 2; } }
+        public override ScaleType ScaleType { get { return ScaleType.Black; } }
+        public override FoodType FavoriteFood { get { return FoodType.Meat | FoodType.Fish; } }
+        public override bool CanFly { get { return true; } }
+
+        public override void GenerateLoot()
+        {
+            this.AddLoot(LootPack.Rich);
+            this.AddLoot(LootPack.MedScrolls, 2);
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+            writer.Write((int)0);
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+            int version = reader.ReadInt();
+        }
+    }
+}
--- Scripts/Mobiles/Normal/GargoyleShade.cs
+++ Scripts/Mobiles/Normal/GargoyleShade.cs
@@ -10,8 +10,8 @@ namespace Server.Mobiles
             : base(AIType.AI_Mage, FightMode.Closest, 10, 1, 0.2, 0.4)
         {
             this.Name = "a gargoyle shade";
-            this.Body = 722;
-            this.Hue = 0x4001;
+            this.Body = 0x4;
+            this.Hue = 16385;
             this.BaseSoundID = 0x482;
 
             this.SetStr(76, 78);
@@ -26,9 +26,9 @@ namespace Server.Mobiles
 
             this.SetDamageType(ResistanceType.Physical, 100);
 
-            this.SetResistance(ResistanceType.Physical, 35, 41);
-            this.SetResistance(ResistanceType.Fire, 40, 44);
-            this.SetResistance(ResistanceType.Cold, 10, 16);
+            this.SetResistance(ResistanceType.Physical, 35, 60);
+            this.SetResistance(ResistanceType.Fire, 40, 50);
+            this.SetResistance(ResistanceType.Cold, 10, 20);
             this.SetResistance(ResistanceType.Poison, 20, 35);
 
             this.SetSkill(SkillName.EvalInt, 57.1, 65.5);
@@ -36,6 +36,7 @@ namespace Server.Mobiles
             this.SetSkill(SkillName.MagicResist, 52.6, 70.0);
             this.SetSkill(SkillName.Tactics, 52.7, 60.0);
             this.SetSkill(SkillName.Wrestling, 47.7, 55.0);
+            this.SetSkill(SkillName.DetectHidden, 30.0, 40.0);
 
             this.Fame = 4000;
             this.Karma = -4000;
