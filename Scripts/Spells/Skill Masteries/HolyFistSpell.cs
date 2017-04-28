0e683fd23982c3cc575c69bc7522696a598612fb
Dexter <kevin_bourne10@yahoo.com>
2017-04-28 11:54:48 -0700

    Additional Cast Effects fixes

--- Scripts/Spells/Skill Masteries/HolyFistSpell.cs
+++ Scripts/Spells/Skill Masteries/HolyFistSpell.cs
@@ -37,6 +37,11 @@ namespace Server.Spells.SkillMasteries
         {
         }
 
+        public override void SendCastEffect()
+        {
+            Caster.FixedEffect(0x37C4, 87, (int)(GetCastDelay().TotalSeconds * 28), 4, 3);
+        }
+
         public override bool CheckCast()
         {
             if (Caster is PlayerMobile && Caster.TithingPoints < RequiredTithing)
