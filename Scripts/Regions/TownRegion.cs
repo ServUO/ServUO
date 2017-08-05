7f257ccd26d373d57bc9f7612dc509a2e07b3e1b
Argalep <argalep@quingis.com>
2017-08-05 16:10:10 +0300

    BattleWarningGump Exploit fix.

--- Scripts/Regions/TownRegion.cs
+++ Scripts/Regions/TownRegion.cs
@@ -1,3 +1,5 @@
+using Server.Engines.VvV;
+using Server.Mobiles;
 using System;
 using System.Xml;
 
@@ -8,6 +10,23 @@ namespace Server.Regions
         public TownRegion(XmlElement xml, Map map, Region parent)
             : base(xml, map, parent)
         {
+        }
+
+        public override void OnExit(Mobile m)
+        {
+            base.OnExit(m);
+
+            if (!(m is PlayerMobile))
+                return;
+
+            if (Map == Map.Felucca && (IsPartOf("Britain") || IsPartOf("Moonglow") || IsPartOf("Britain") || IsPartOf("Jhelom") || IsPartOf("Yew") || IsPartOf("Minoc") || IsPartOf("Trinsic") ||
+                IsPartOf("SkaraBrae") || IsPartOf("NewMagincia") || IsPartOf("Vesper")))
+            {
+                if (m.HasGump(typeof(BattleWarningGump)))
+                {
+                    m.CloseGump(typeof(BattleWarningGump));
+                }
+            }
         }
     }
 }
