8a80fd72c0776078dd90f5728b6d78458546f1bc
Argalep <argalep@quingis.com>
2017-08-29 23:14:42 +0300

    Quest Gump Fix.

--- Scripts/Services/Quests/QuestConversation.cs
+++ Scripts/Services/Quests/QuestConversation.cs
@@ -14,57 +14,35 @@ namespace Server.Engines.Quests
         }
 
         public abstract object Message { get; }
-        public virtual QuestItemInfo[] Info
-        {
-            get
-            {
-                return null;
-            }
-        }
-        public virtual bool Logged
-        {
-            get
-            {
-                return true;
-            }
-        }
+        public virtual QuestItemInfo[] Info { get { return null; } }
+        public virtual bool Logged { get { return true; } }
+
         public QuestSystem System
         {
-            get
-            {
-                return this.m_System;
-            }
-            set
-            {
-                this.m_System = value;
-            }
+            get { return m_System; }
+            set { m_System = value; }
         }
         public bool HasBeenRead
         {
-            get
-            {
-                return this.m_HasBeenRead;
-            }
-            set
-            {
-                this.m_HasBeenRead = value;
-            }
+            get { return m_HasBeenRead; }
+            set { m_HasBeenRead = value; }
         }
+
         public virtual void BaseDeserialize(GenericReader reader)
         {
             int version = reader.ReadEncodedInt();
 
-            switch ( version )
+            switch (version)
             {
                 case 0:
                     {
-                        this.m_HasBeenRead = reader.ReadBool();
+                        m_HasBeenRead = reader.ReadBool();
 
                         break;
                     }
             }
 
-            this.ChildDeserialize(reader);
+            ChildDeserialize(reader);
         }
 
         public virtual void ChildDeserialize(GenericReader reader)
@@ -76,9 +54,9 @@ namespace Server.Engines.Quests
         {
             writer.WriteEncodedInt((int)0); // version
 
-            writer.Write((bool)this.m_HasBeenRead);
+            writer.Write((bool)m_HasBeenRead);
 
-            this.ChildSerialize(writer);
+            ChildSerialize(writer);
         }
 
         public virtual void ChildSerialize(GenericWriter writer)
@@ -102,38 +80,39 @@ namespace Server.Engines.Quests
         public QuestConversationsGump(ArrayList conversations)
             : base(30, 50)
         {
-            this.m_Conversations = conversations;
+            m_Conversations = conversations;
 
-            this.Closable = false;
+            Closable = false;
 
-            this.AddPage(0);
+            AddPage(0);
 
-            this.AddImage(349, 10, 9392);
-            this.AddImageTiled(349, 130, 100, 120, 9395);
-            this.AddImageTiled(149, 10, 200, 140, 9391);
-            this.AddImageTiled(149, 250, 200, 140, 9397);
-            this.AddImage(349, 250, 9398);
-            this.AddImage(35, 10, 9390);
-            this.AddImageTiled(35, 150, 120, 100, 9393);
-            this.AddImage(35, 250, 9396);
+            AddImage(349, 10, 9392);
+            AddImageTiled(149, 10, 200, 260, 9394);
+            AddImageTiled(349, 130, 114, 120, 9395);
+            AddImageTiled(149, 10, 200, 140, 9391);
+            AddImageTiled(149, 250, 200, 140, 9397);
+            AddImage(349, 250, 9398);
+            AddImage(35, 10, 9390);
+            AddImageTiled(35, 130, 114, 120, 9393);
+            AddImage(35, 250, 9396);
 
-            this.AddHtmlLocalized(110, 60, 200, 20, 1049069, White, false, false); // <STRONG>Conversation Event</STRONG>
+            AddHtmlLocalized(110, 60, 200, 20, 1049069, White, false, false); // <STRONG>Conversation Event</STRONG>
 
-            this.AddImage(65, 14, 10102);
-            this.AddImageTiled(81, 14, 349, 17, 10101);
-            this.AddImage(426, 14, 10104);
+            AddImage(65, 14, 10102);
+            AddImageTiled(81, 14, 349, 17, 10101);
+            AddImage(426, 14, 10104);
 
-            this.AddImageTiled(55, 40, 388, 323, 2624);
-            this.AddAlphaRegion(55, 40, 388, 323);
+            AddImageTiled(55, 40, 388, 323, 2624);
+            AddAlphaRegion(55, 40, 388, 323);
 
-            this.AddImageTiled(75, 90, 200, 1, 9101);
-            this.AddImage(75, 58, 9781);
-            this.AddImage(380, 45, 223);
+            AddImageTiled(75, 90, 200, 1, 9101);
+            AddImage(75, 58, 9781);
+            AddImage(380, 45, 223);
 
-            this.AddButton(220, 335, 2313, 2312, 1, GumpButtonType.Reply, 0);
-            this.AddImage(0, 0, 10440);
+            AddButton(220, 335, 2313, 2312, 1, GumpButtonType.Reply, 0);
+            AddImage(0, 0, 10440);
 
-            this.AddPage(1);
+            AddPage(1);
 
             for (int i = 0; i < conversations.Count; ++i)
             {
@@ -141,27 +120,27 @@ namespace Server.Engines.Quests
 
                 if (i > 0)
                 {
-                    this.AddButton(65, 366, 9909, 9911, 0, GumpButtonType.Page, 1 + i);
-                    this.AddHtmlLocalized(90, 367, 50, 20, 1043354, Black, false, false); // Previous
+                    AddButton(65, 366, 9909, 9911, 0, GumpButtonType.Page, 1 + i);
+                    AddHtmlLocalized(90, 367, 50, 20, 1043354, Black, false, false); // Previous
 
-                    this.AddPage(1 + i);
+                    AddPage(1 + i);
                 }
 
-                this.AddHtmlObject(70, 110, 365, 220, conv.Message, LightGreen, false, true);
+                AddHtmlObject(70, 110, 365, 220, conv.Message, LightGreen, false, true);
 
                 if (i > 0)
                 {
-                    this.AddButton(420, 366, 9903, 9905, 0, GumpButtonType.Page, i);
-                    this.AddHtmlLocalized(370, 367, 50, 20, 1043353, Black, false, false); // Next
+                    AddButton(420, 366, 9903, 9905, 0, GumpButtonType.Page, i);
+                    AddHtmlLocalized(370, 367, 50, 20, 1043353, Black, false, false); // Next
                 }
             }
         }
 
         public override void OnResponse(NetState sender, RelayInfo info)
         {
-            for (int i = this.m_Conversations.Count - 1; i >= 0; --i)
+            for (int i = m_Conversations.Count - 1; i >= 0; --i)
             {
-                QuestConversation qc = (QuestConversation)this.m_Conversations[i];
+                QuestConversation qc = (QuestConversation)m_Conversations[i];
 
                 if (!qc.HasBeenRead)
                 {
