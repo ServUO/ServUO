028ee2fb38373ba157cbb8b9909037efe18e5c31
Dexter <kevin_bourne10@yahoo.com>
2017-09-14 21:35:25 -0700

    error...Fixed

--- Scripts/Abilities/FrenziedWhirlwind.cs
+++ Scripts/Abilities/FrenziedWhirlwind.cs
@@ -86,9 +86,8 @@ namespace Server.Items
                 {
                     defender.Send(SpeedControl.WalkSpeed);
                     BuffInfo.AddBuff(defender, new BuffInfo(BuffIcon.SplinteringEffect, 1153804, 1152144, TimeSpan.FromSeconds(2.0), defender));
+                    Timer.DelayCall<Mobile>(TimeSpan.FromSeconds(2), mob => mob.Send(SpeedControl.Disable), defender);
                 }
-
-                m_Registry[attacker] = new InternalTimer(attacker, targets);
             }
         }
 
