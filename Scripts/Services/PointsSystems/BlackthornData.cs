67ef52aabb8940fd9f4d624548ef1c2cee3f4120
Tasanar <knight.daniel.r@gmail.com>
2018-01-10 07:54:06 -0500

    Update BlackthornData.cs
    Fixes an exploit where players can bring a bonded pet into Blackthorns dungeon and kill / rez it over and over, very quickly, to farm blackthorn artifacts,

--- Scripts/Services/PointsSystems/BlackthornData.cs
+++ Scripts/Services/PointsSystems/BlackthornData.cs
@@ -29,6 +29,9 @@ namespace Server.Engines.Points
 
         public override void ProcessKill(BaseCreature victim, Mobile damager, int index)
         {
+            if (victim.Controlled || victim.Owners.Count > 0 || victim.Fame <= 0)
+                return;
+        
             Region r = victim.Region;
 
             if (damager is PlayerMobile && r.IsPartOf("BlackthornDungeon"))
