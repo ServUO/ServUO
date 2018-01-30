170113f78e028ba074cbdebba9f7ac76ba21a6aa
Tasanar <knight.daniel.r@gmail.com>
2018-01-30 15:57:15 -0500

    Update Malefic.cs
    Since this derives from DreadSpider.cs it could be tamed and it should not be.
    
    Added in the Tamable check.

--- Scripts/Mobiles/Named/Malefic.cs
+++ Scripts/Mobiles/Named/Malefic.cs
@@ -9,41 +9,43 @@ namespace Server.Mobiles
         [Constructable]
         public Malefic()
         {
-            this.Name = "Malefic";
-            this.Hue = 0x455;
+            Name = "Malefic";
+            Hue = 0x455;
 
-            this.SetStr(210, 284);
-            this.SetDex(153, 197);
-            this.SetInt(349, 390);
+            SetStr(210, 284);
+            SetDex(153, 197);
+            SetInt(349, 390);
 
-            this.SetHits(600, 747);
-            this.SetStam(153, 197);
-            this.SetMana(349, 390);
+            SetHits(600, 747);
+            SetStam(153, 197);
+            SetMana(349, 390);
 
-            this.SetDamage(15, 22);
+            SetDamage(15, 22);
 
-            this.SetDamageType(ResistanceType.Physical, 20);
-            this.SetDamageType(ResistanceType.Poison, 80);
+            SetDamageType(ResistanceType.Physical, 20);
+            SetDamageType(ResistanceType.Poison, 80);
 
-            this.SetResistance(ResistanceType.Physical, 60, 70);
-            this.SetResistance(ResistanceType.Fire, 40, 50);
-            this.SetResistance(ResistanceType.Cold, 40, 49);
-            this.SetResistance(ResistanceType.Poison, 100);
-            this.SetResistance(ResistanceType.Energy, 41, 48);
+            SetResistance(ResistanceType.Physical, 60, 70);
+            SetResistance(ResistanceType.Fire, 40, 50);
+            SetResistance(ResistanceType.Cold, 40, 49);
+            SetResistance(ResistanceType.Poison, 100);
+            SetResistance(ResistanceType.Energy, 41, 48);
 
-            this.SetSkill(SkillName.Wrestling, 96.9, 112.4);
-            this.SetSkill(SkillName.Tactics, 91.3, 105.4);
-            this.SetSkill(SkillName.MagicResist, 79.8, 95.1);
-            this.SetSkill(SkillName.Magery, 103.0, 118.6);
-            this.SetSkill(SkillName.EvalInt, 105.7, 119.6);
-            this.SetSkill(SkillName.Meditation, 0);
+            SetSkill(SkillName.Wrestling, 96.9, 112.4);
+            SetSkill(SkillName.Tactics, 91.3, 105.4);
+            SetSkill(SkillName.MagicResist, 79.8, 95.1);
+            SetSkill(SkillName.Magery, 103.0, 118.6);
+            SetSkill(SkillName.EvalInt, 105.7, 119.6);
+            SetSkill(SkillName.Meditation, 0);
 
-            this.Fame = 21000;
-            this.Karma = -21000;
+            Fame = 21000;
+            Karma = -21000;
+            
+            Tamable = false;
 
             for (int i = 0; i < Utility.RandomMinMax(0, 1); i++)
             {
-                this.PackItem(Loot.RandomScroll(0, Loot.ArcanistScrollTypes.Length, SpellbookType.Arcanist));
+                PackItem(Loot.RandomScroll(0, Loot.ArcanistScrollTypes.Length, SpellbookType.Arcanist));
             }
 
             /*
@@ -67,7 +69,7 @@ namespace Server.Mobiles
         }
         public override void GenerateLoot()
         {
-            this.AddLoot(LootPack.UltraRich, 3);
+            AddLoot(LootPack.UltraRich, 3);
         }
 
         public override WeaponAbility GetWeaponAbility()
