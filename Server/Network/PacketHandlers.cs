f647092dd7a6fd8255d1df8f3824ab8ec2a3b7d2
VitaNex <admin@vita-nex.com>
2018-02-17 17:41:52 +0000

    + Core GlobalUpdateRange and GlobalMaxUpdateRange must NOT be equal; * The former acts as a minimum while the latter acts as a maximum. * If these two values are equal, or the former is higher than the lowest possible individual mobile update range, weird things can and will happen.

--- Server/Network/PacketHandlers.cs
+++ Server/Network/PacketHandlers.cs
@@ -1738,6 +1738,9 @@ namespace Server.Network
 
 			int range = pvSrc.ReadByte();
 
+			// Don't let range drop below the minimum standard.
+			range = Math.Max(Core.GlobalUpdateRange, range);
+			
             int old = state.UpdateRange;
 
 			if (old == range)
