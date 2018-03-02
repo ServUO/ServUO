f2800ed6b717cd70562b2c28d5077529ca257cfa
Tasanar <knight.daniel.r@gmail.com>
2018-03-02 09:48:08 -0500

    Update ConjurersGarbHarbringer.cs

--- Scripts/Items/Artifacts/Equipment/Clothing/ConjurersGarbHarbringer.cs
+++ Scripts/Items/Artifacts/Equipment/Clothing/ConjurersGarbHarbringer.cs
@@ -6,8 +6,7 @@ namespace Server.Items
     public class ConjureresGarbHarbringer : BaseOuterTorso, ITokunoDyable
 	{
 		public override bool IsArtifact { get { return true; } }
-        public override bool CanBeWornByGargoyles { get { return true; } }
-        
+        public override bool CanBeWornByGargoyles { get { return true; } }       
         [Constructable]
         public ConjureresGarbHarbringer()
             : base(0x1F03, 0x486)
