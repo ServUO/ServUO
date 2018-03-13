5b4364ef403c8641ff57be85378bfb8e570b370f
Andre Zaiats <azaiats@gmail.com>
2018-03-13 10:41:21 -0300

    Legendary/Elder/Mythical Detective of the Royal Guard [replica] artifact boots added

---
+++ Scripts/Items/Artifacts/Equipment/Clothing/ElderDetectiveoftheRoyalGuard.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Server.Items
+{
+    public class ElderDetectiveoftheRoyalGuard : Boots
+	{
+		public override bool IsArtifact { get { return true; } }
+        [Constructable]
+        public ElderDetectiveoftheRoyalGuard()
+            : base(0x170B)
+        { 
+            this.Hue = 902;
+            
+            this.Attributes.BonusInt = 3;
+        }
+
+        public ElderDetectiveoftheRoyalGuard(Serial serial)
+            : base(serial)
+        {
+        }
+
+        public override int InitMinHits
+        {
+            get
+            {
+                return 150;
+            }
+        }
+        public override int InitMaxHits
+        {
+            get
+            {
+                return 150;
+            }
+        }
+        public override int LabelNumber
+        {
+            get
+            {
+                return 1094895;
+            }
+        }// Elder Detective of the Royal Guard [Replica]
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+
+            int version = reader.ReadInt();
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+
+            writer.Write((int)0);//version
+        }
+    }
+}
---
+++ Scripts/Items/Artifacts/Equipment/Clothing/MythicalDetectiveoftheRoyalGuard.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Server.Items
+{
+    public class MythicalDetectiveoftheRoyalGuard : Boots
+	{
+		public override bool IsArtifact { get { return true; } }
+        [Constructable]
+        public MythicalDetectiveoftheRoyalGuard()
+            : base(0x170B)
+        { 
+            this.Hue = 902;
+            
+            this.Attributes.BonusInt = 4;
+        }
+
+        public MythicalDetectiveoftheRoyalGuard(Serial serial)
+            : base(serial)
+        {
+        }
+
+        public override int InitMinHits
+        {
+            get
+            {
+                return 150;
+            }
+        }
+        public override int InitMaxHits
+        {
+            get
+            {
+                return 150;
+            }
+        }
+        public override int LabelNumber
+        {
+            get
+            {
+                return 1094896;
+            }
+        }// Mythical Detective of the Royal Guard [Replica]
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+
+            int version = reader.ReadInt();
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+
+            writer.Write((int)0);//version
+        }
+    }
+}
