d49ef08f361d4e54bc995706f5c5ced9a1576175
Argalep <argalep@quingis.com>
2018-06-01 00:14:04 +0300

    * Covetous creatures add slayer group

--- Scripts/Abilities/SlayerGroup.cs
+++ Scripts/Abilities/SlayerGroup.cs
@@ -77,7 +77,9 @@ namespace Server.Items
                     typeof(GrayGoblinMage), typeof(GrayGoblinKeeper),
                     typeof(GrayGoblin), typeof(GreenGoblinAlchemistRenowned),
                     typeof(GrayGoblinMageRenowned), typeof(CorgulTheSoulBinder),
-                    typeof(PirateCrew)
+                    typeof(PirateCrew), typeof(LizardmanWitchdoctor),
+                    typeof(OrcFootSoldier), typeof(RatmanAssassin),
+                    typeof(OgreBoneCrusher), typeof(TitanRockHunter)
                 );
 
             humanoid.Entries = new SlayerEntry[]
@@ -87,7 +89,7 @@ namespace Server.Items
                         SlayerName.OgreTrashing,
 
                         typeof(Ogre),               typeof(OgreLord),
-                        typeof(ArcticOgreLord)
+                        typeof(ArcticOgreLord),     typeof(OgreBoneCrusher)
                     ),
 
                 new SlayerEntry
@@ -139,7 +141,9 @@ namespace Server.Items
                     typeof(EffeteUndeadGargoyle), typeof(UndeadGargoyle),
                     typeof(UndeadGuardian), typeof(PutridUndeadGargoyle),
                     typeof(PutridUndeadGuardian), typeof(Juonar),
-                    typeof(Spellbinder)
+                    typeof(Spellbinder), typeof(AngeredSpirit),
+                    typeof(BoneSwordSlinger), typeof(CovetousRevenant),
+                    typeof(DiseasedLich), typeof(VileCadaver)
                 );
 
             undead.Entries = new SlayerEntry[0];
@@ -225,14 +229,16 @@ namespace Server.Items
                             typeof(EarthElemental),     typeof(SummonedEarthElemental),
                             typeof(GoldenElemental),    typeof(ShadowIronElemental),
                             typeof(ValoriteElemental),  typeof(VeriteElemental),
-                            typeof(QuartzElemental),    typeof(DemonKnight)
+                            typeof(QuartzElemental),    typeof(DemonKnight),
+                            typeof(CovetousEarthElemental)
                         ),
 
                     new SlayerEntry
                         (
                             SlayerName.ElementalHealth,
 
-                            typeof(PoisonElemental),    typeof(DemonKnight)
+                            typeof(PoisonElemental),    typeof(DemonKnight),
+                            typeof(VenomElemental)
                         ),
 
                     new SlayerEntry
@@ -241,7 +247,7 @@ namespace Server.Items
 
                             typeof(FireElemental),          typeof(FireElementalRenowned),
                             typeof(SummonedFireElemental),  typeof(FlameElemental),
-                            typeof(DemonKnight)
+                            typeof(DemonKnight),            typeof(SearingElemental)
                         ),
 
                     new SlayerEntry
@@ -260,7 +266,7 @@ namespace Server.Items
                             typeof(Flurry),         typeof(Mistral),
                             typeof(Tempest),        typeof(UnboundEnergyVortex),
                             typeof(ChaosVortex),    typeof(WindElemental),
-                            typeof(DemonKnight)
+                            typeof(DemonKnight),    typeof(VortexElemental)
                         ),
 
                     new SlayerEntry
@@ -268,7 +274,7 @@ namespace Server.Items
                             SlayerName.WaterDissipation,
 
                             typeof(WaterElemental),     typeof(SummonedWaterElemental),
-                            typeof(DemonKnight)
+                            typeof(DemonKnight),        typeof(CovetousWaterElemental)
                         )
                 };
 
@@ -327,7 +333,9 @@ namespace Server.Items
                             typeof(Gargoyle),               typeof(GargoyleDestroyer),
                             typeof(GargoyleEnforcer),       typeof(StoneGargoyle),
                             typeof(GargoyleShade),          typeof(EffetePutridGargoyle),
-                            typeof(EffeteUndeadGargoyle)
+                            typeof(EffeteUndeadGargoyle),   typeof(DaemonMongbat),
+                            typeof(CovetousDoppleganger),   typeof(CovetousFireDaemon),
+                            typeof(GargoyleAssassin),       typeof(LesserOni)
                         ),
                 };
             }
