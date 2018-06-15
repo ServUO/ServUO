118571afafd8e8e1c863d7398376214157dc8f5a
Tasanar <knight.daniel.r@gmail.com>
2018-06-15 19:25:49 -0400

    Add files via upload
    https://www.servuo.com/threads/11th-anniversary-collection.6920/
    
    Again ALL credit goes to Shazzy. This item is also EA exact. 
    
    http://www.uoguide.com/11th_Year_Collection

---
+++ Scripts/Items/Addons/MapleTree.cs
@@ -0,0 +1,138 @@
+using System;
+using Server;
+using Server.Gumps;
+using Server.Network;
+
+namespace Server.Items
+{
+	public class MapleTreeAddon : BaseAddon
+	{
+		public override BaseAddonDeed Deed { get { return new MapleTreeDeed(); } }
+
+		[Constructable]
+		public MapleTreeAddon( bool trunk )
+		{
+			AddComponent( new LocalizedAddonComponent( 0x247D, 1071104 ), 0, 0, 0 );
+
+			if ( !trunk )
+				AddComponent( new LocalizedAddonComponent( 0x36A1, 1071104 ), 0, 0, 0 );
+		}
+
+		public MapleTreeAddon( Serial serial )
+			: base( serial )
+		{
+		}
+
+		public override void Serialize( GenericWriter writer )
+		{
+			base.Serialize( writer );
+
+			writer.WriteEncodedInt( 0 ); // version
+		}
+
+		public override void Deserialize( GenericReader reader )
+		{
+			base.Deserialize( reader );
+
+			/*int version = */
+			reader.ReadEncodedInt();
+		}
+	}
+
+	public class MapleTreeDeed : BaseAddonDeed
+	{
+		public override BaseAddon Addon { get { return new MapleTreeAddon( m_Trunk ); } }
+		public override int LabelNumber { get { return 1071104; } } // Maple Tree
+
+		private bool m_Trunk;
+
+		[Constructable]
+		public MapleTreeDeed()
+		{
+			LootType = LootType.Blessed;
+		}
+
+		public MapleTreeDeed( Serial serial )
+			: base( serial )
+		{
+		}
+
+		public override void OnDoubleClick( Mobile from )
+		{
+			if ( IsChildOf( from.Backpack ) )
+			{
+				from.CloseGump( typeof( InternalGump ) );
+				from.SendGump( new InternalGump( this ) );
+			}
+			else
+				from.SendLocalizedMessage( 1062334 ); // This item must be in your backpack to be used.
+		}
+
+		private void SendTarget( Mobile m )
+		{
+			base.OnDoubleClick( m );
+		}
+
+		public override void Serialize( GenericWriter writer )
+		{
+			base.Serialize( writer );
+
+			writer.WriteEncodedInt( 0 ); // version
+		}
+
+		public override void Deserialize( GenericReader reader )
+		{
+			base.Deserialize( reader );
+
+			/*int version = */
+			reader.ReadEncodedInt();
+		}
+
+		private class InternalGump : Gump
+		{
+			//public override int TypeID { get { return 0x2341; } }
+
+			private MapleTreeDeed m_Deed;
+
+			public InternalGump( MapleTreeDeed deed )
+				: base( 60, 36 )
+			{
+				m_Deed = deed;
+
+				AddPage( 0 );
+
+				AddBackground( 0, 0, 273, 324, 0x13BE );
+				AddImageTiled( 10, 10, 253, 20, 0xA40 );
+				AddImageTiled( 10, 40, 253, 244, 0xA40 );
+				AddImageTiled( 10, 294, 253, 20, 0xA40 );
+				AddAlphaRegion( 10, 10, 253, 304 );
+
+				AddButton( 10, 294, 0xFB1, 0xFB2, 0, GumpButtonType.Reply, 0 );
+				AddHtmlLocalized( 45, 296, 450, 20, 1060051, 0x7FFF, false, false ); // CANCEL
+
+				AddHtmlLocalized( 14, 12, 273, 20, 1076170, 0x7FFF, false, false ); // Choose Direction
+
+				AddPage( 1 );
+
+				AddButton( 19, 49, 0x845, 0x846, 100, GumpButtonType.Reply, 0 );
+				AddHtmlLocalized( 44, 47, 213, 20, 1071104, 0x7FFF, false, false ); // Maple Tree
+				AddButton( 19, 73, 0x845, 0x846, 101, GumpButtonType.Reply, 0 );
+				AddHtmlLocalized( 44, 71, 213, 20, 1071301, 0x7FFF, false, false ); // Maple Tree (trunk)
+			}
+
+			public override void OnResponse( Server.Network.NetState sender, RelayInfo info )
+			{
+				if ( m_Deed == null || m_Deed.Deleted )
+					return;
+
+				int buttonId = info.ButtonID;
+
+				if ( buttonId == 100 || buttonId == 101 )
+				{
+					m_Deed.m_Trunk = ( buttonId == 101 );
+					m_Deed.SendTarget( sender.Mobile );
+				}
+			}
+		}
+	}
+}
