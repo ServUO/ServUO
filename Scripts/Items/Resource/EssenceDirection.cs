8dceff129b76c966e890c8744ac48606c558f7a7
Tasanar <knight.daniel.r@gmail.com>
2018-06-16 14:00:11 -0400

    Update EssenceDirection.cs

--- Scripts/Items/Resource/EssenceDirection.cs
+++ Scripts/Items/Resource/EssenceDirection.cs
@@ -14,9 +14,9 @@ namespace Server.Items
         public EssenceDirection(int amount)
             : base(0x571C)
         {
-            this.Stackable = true;
-            this.Amount = amount;
-			this.Hue = 1156;
+            Stackable = true;
+            Amount = amount;
+			Hue = 1156;
         }
 
         public EssenceDirection(Serial serial)
