60d81fce5b1f5f214a02cdc89af9ee1954a63011
Dexter <kevin_bourne10@yahoo.com>
2018-06-19 09:24:28 -0700

    Update NameChangeToken.cs

--- Scripts/Items/StoreBought/NameChangeToken.cs
+++ Scripts/Items/StoreBought/NameChangeToken.cs
@@ -96,7 +96,7 @@ namespace Server.Items
 
         public override void OnResponse(RelayInfo info)
         {
-            if (info.ButtonID == 1)
+            if (info.ButtonID == 1 && Token.IsChildOf(User.Backpack))
             {
                 TextRelay relay = info.GetTextEntry(0);
 
