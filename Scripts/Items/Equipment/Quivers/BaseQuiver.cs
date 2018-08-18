f35deeab0a3f57664f47d68b97a634a6e991a5b7
Dexter <kevin_bourne10@yahoo.com>
2018-08-18 09:21:14 -0700

    Bug Fixes - Removed redundant Open Backpack context menu entry from playermobile - Fixed issue where you could add too much ammo to quivers

--- Scripts/Items/Equipment/Quivers/BaseQuiver.cs
+++ Scripts/Items/Equipment/Quivers/BaseQuiver.cs
@@ -352,6 +352,7 @@ namespace Server.Items
             }
 
             Item ammo = Ammo;
+
             if(ammo != null && ammo.Amount > 0)
             {
                 if (IsArrowAmmo && item is Bolt)
@@ -369,14 +370,27 @@ namespace Server.Items
 
                 if (item.Amount + currentAmount <= m_Capacity)
                     return base.CheckHold(m, item, message, checkItems, plusItems, plusWeight);
+            }
 
+            return false;
+        }
+
+        public override bool CheckStack(Mobile from, Item item)
+        {
+            if (!CheckType(item))
+            {
                 return false;
             }
-            else if (checkItems)
-                return false;
 
-            if (ammo == null || ammo.Deleted)
-                return false;
+            Item ammo = Ammo;
+
+            if (ammo != null)
+            {
+                int currentAmount = Items.Sum(i => i.Amount);
+
+                if (item.Amount + currentAmount <= m_Capacity)
+                    return base.CheckStack(from, item);
+            }
 
             return false;
         }
--- Scripts/Mobiles/PlayerMobile.cs
+++ Scripts/Mobiles/PlayerMobile.cs
@@ -2332,11 +2332,6 @@ namespace Server.Mobiles
                     list.Add(new Engines.Points.LoyaltyRating(this));
 			    }
 
-                if (Backpack != null && CanSee(Backpack) && Alive)
-                {
-                    list.Add(new OpenBackpackEntry(this));
-                }
-
 				if (Alive && InsuranceEnabled)
 				{
 					list.Add(new CallbackEntry(6201, ToggleItemInsurance));
