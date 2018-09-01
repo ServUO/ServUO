c780429b31f89bafd3b28a3450b4cd75d7186035
Argalep <argalep@quingis.com>
2018-09-01 23:22:21 +0300

    * Acidslug and Slimeler located underworld can not leave the pits * Spell Channeling weapon, the durability of the weapon should not decrease.

--- Scripts/Items/Equipment/Weapons/BaseWeapon.cs
+++ Scripts/Items/Equipment/Weapons/BaseWeapon.cs
@@ -2166,7 +2166,7 @@ namespace Server.Items
             }
 
             double chance = NegativeAttributes.Antique > 0 ? 5 : 0;
-            bool acidicTarget = MaxRange <= 1 && m_AosAttributes.SpellChanneling > 0 && !(this is Fists) && (defender is Slime || defender is ToxicElemental || defender is CorrosiveSlime);
+            bool acidicTarget = MaxRange <= 1 && m_AosAttributes.SpellChanneling == 0 && !(this is Fists) && (defender is Slime || defender is ToxicElemental || defender is CorrosiveSlime);
 
             if (acidicTarget || (defender != null && splintering) || Utility.Random(40) <= chance)
             {
