6a5efada93c335003c7a51dbd6fa598e21925423
Tasanar <knight.daniel.r@gmail.com>
2018-10-11 10:40:20 -0400

    Goblin Sound FIx
    Goblins, which were just copied from Orcs when added, were using Orc sounds. This cleans up the code a little and adds the correct sounds to Goblins.

--- Scripts/Mobiles/Normal/EnslavedGoblinKeeper.cs
+++ Scripts/Mobiles/Normal/EnslavedGoblinKeeper.cs
@@ -10,113 +10,95 @@ namespace Server.Mobiles
         public EnslavedGoblinKeeper()
             : base(AIType.AI_Melee, FightMode.Closest, 10, 1, 0.2, 0.4)
         {
-            this.Name = "Enslaved Goblin Keeper";
-            this.Body = 334;
-            this.BaseSoundID = 0x45A;
+            Name = "Enslaved Goblin Keeper";
+            Body = 334;
+            BaseSoundID = 0x600;
 
-            this.SetStr(297, 297);
-            this.SetDex(80, 80);
-            this.SetInt(118, 118);
+            SetStr(297, 297);
+            SetDex(80, 80);
+            SetInt(118, 118);
 
-            this.SetHits(174, 174);
-            this.SetStam(80, 80);
-            this.SetMana(118, 118);
+            SetHits(174, 174);
+            SetStam(80, 80);
+            SetMana(118, 118);
 
-            this.SetDamage(5, 7);
+            SetDamage(5, 7);
 
-            this.SetDamageType(ResistanceType.Physical, 100);
+            SetDamageType(ResistanceType.Physical, 100);
 
-            this.SetResistance(ResistanceType.Physical, 47, 47);
-            this.SetResistance(ResistanceType.Fire, 37, 37);
-            this.SetResistance(ResistanceType.Cold, 29, 29);
-            this.SetResistance(ResistanceType.Poison, 10, 11);
-            this.SetResistance(ResistanceType.Energy, 19, 19);
+            SetResistance(ResistanceType.Physical, 47, 47);
+            SetResistance(ResistanceType.Fire, 37, 37);
+            SetResistance(ResistanceType.Cold, 29, 29);
+            SetResistance(ResistanceType.Poison, 10, 11);
+            SetResistance(ResistanceType.Energy, 19, 19);
 
-            this.SetSkill(SkillName.MagicResist, 121.6, 122.2);
-            this.SetSkill(SkillName.Tactics, 80.0, 82.8);
-            this.SetSkill(SkillName.Anatomy, 82.0, 84.8);
-            this.SetSkill(SkillName.Wrestling, 99.2, 100.7);
+            SetSkill(SkillName.MagicResist, 121.6, 122.2);
+            SetSkill(SkillName.Tactics, 80.0, 82.8);
+            SetSkill(SkillName.Anatomy, 82.0, 84.8);
+            SetSkill(SkillName.Wrestling, 99.2, 100.7);
 
-            this.Fame = 1500;
-            this.Karma = -1500;
+            Fame = 1500;
+            Karma = -1500;
 
-            this.VirtualArmor = 28;
+            VirtualArmor = 28;
 
             // Loot - 30-40gold, magicitem,gem,goblin blood, essence control
             switch ( Utility.Random(20) )
             {
                 case 0:
-                    this.PackItem(new Scimitar());
+                    PackItem(new Scimitar());
                     break;
                 case 1:
-                    this.PackItem(new Katana());
+                    PackItem(new Katana());
                     break;
                 case 2:
-                    this.PackItem(new WarMace());
+                    PackItem(new WarMace());
                     break;
                 case 3:
-                    this.PackItem(new WarHammer());
+                    PackItem(new WarHammer());
                     break;
                 case 4:
-                    this.PackItem(new Kryss());
+                    PackItem(new Kryss());
                     break;
                 case 5:
-                    this.PackItem(new Pitchfork());
+                    PackItem(new Pitchfork());
                     break;
             }
 
-            this.PackItem(new ThighBoots());
+            PackItem(new ThighBoots());
 
             switch ( Utility.Random(3) )
             {
                 case 0:
-                    this.PackItem(new Ribs());
+                    PackItem(new Ribs());
                     break;
                 case 1:
-                    this.PackItem(new Shaft());
+                    PackItem(new Shaft());
                     break;
                 case 2:
-                    this.PackItem(new Candle());
+                    PackItem(new Candle());
                     break;
             }
 
             if (0.2 > Utility.RandomDouble())
-                this.PackItem(new BolaBall());
+                PackItem(new BolaBall());
         }
 
         public EnslavedGoblinKeeper(Serial serial)
             : base(serial)
         {
         }
+		
+		public override int GetAngerSound() { return 0x600; }
+        public override int GetIdleSound() { return 0x600; }
+        public override int GetAttackSound() { return 0x5FD; }
+        public override int GetHurtSound() { return 0x5FF; }
+        public override int GetDeathSound() { return 0x5FE; }
 
-        public override bool CanRummageCorpses
-        {
-            get
-            {
-                return true;
-            }
-        }
-        public override int TreasureMapLevel
-        {
-            get
-            {
-                return 1;
-            }
-        }
-        public override int Meat
-        {
-            get
-            {
-                return 1;
-            }
-        }
-        public override OppositionGroup OppositionGroup
-        {
-            get
-            {
-                return OppositionGroup.SavagesAndOrcs;
-            }
-        }
+        public override bool CanRummageCorpses { get { return true; } }
+        public override int TreasureMapLevel { get { return 1; } }
+        public override int Meat { get { return 1; } }
+        public override OppositionGroup OppositionGroup { get { return OppositionGroup.SavagesAndOrcs; } }
 
         public override void GenerateLoot()
         {
