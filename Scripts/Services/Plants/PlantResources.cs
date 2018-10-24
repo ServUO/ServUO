a1e341ea95df0a689c45212fdded22abf9a4efd2
Dexter <kevin_bourne10@yahoo.com>
2018-10-24 12:37:22 -0700

    Update PlantResources.cs
    Added Resource BarkFragment to PoppyPatch and FlaxBundle to Flax.

--- Scripts/Services/Plants/PlantResources.cs
+++ Scripts/Services/Plants/PlantResources.cs
@@ -17,10 +17,12 @@ namespace Server.Engines.Plants
             new PlantResourceInfo(PlantType.BarrelCactus, PlantHue.BrightGreen, typeof(GreenThorns)),
             new PlantResourceInfo(PlantType.CocoaTree, PlantHue.Plain, typeof(CocoaPulp)),
             new PlantResourceInfo(PlantType.SugarCanes, PlantHue.Plain, typeof(SackOfSugar)),
-            new PlantResourceInfo(PlantType.FlaxFlowers, PlantHue.Plain, typeof(Cotton)),
+            new PlantResourceInfo(PlantType.FlaxFlowers, PlantHue.Plain, typeof(FlaxBundle)),
             new PlantResourceInfo(PlantType.CypressStraight, PlantHue.Plain, typeof(BarkFragment)),
             new PlantResourceInfo(PlantType.CypressTwisted, PlantHue.Plain, typeof(BarkFragment)),
+            new PlantResourceInfo(PlantType.PoppyPatch, PlantHue.Plain, typeof(BarkFragment))
             new PlantResourceInfo(PlantType.Vanilla, PlantHue.Plain, typeof(Vanilla))
+            
         };
 
         private readonly PlantType m_PlantType;
