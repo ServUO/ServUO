aae71ac0fc9054c3616ece20d18af322d195408b
Dexter <kevin_bourne10@yahoo.com>
2018-10-27 12:30:01 -0700

    Added missing resource!

---
+++ Scripts/Items/Resource/Blackrock.cs
@@ -0,0 +1,47 @@
+using System;
+using Server.Network;
+
+namespace Server.Items
+{
+    public class Blackrock : Item
+    {
+        [Constructable]
+        public Blackrock()
+            : base(Utility.RandomList(0x136C, 0x1EA7))
+        {
+            Hue = 1954;
+        }
+
+        public Blackrock(Serial serial)
+            : base(serial)
+        {
+        }
+
+        public override int LabelNumber
+        {
+            get
+            {
+                switch (ItemID)
+                {
+                    case 0x136C: return 1153837; // a large piece of blackrock
+                    case 0x1EA7: return 1150016; // a small piece of blackrock
+                    default: return 1153836; // a piece of blackrock
+                }
+            }
+        }
+		
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+
+            writer.Write((int)0); // version
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+
+            int version = reader.ReadInt();
+        }
+    }
+}
