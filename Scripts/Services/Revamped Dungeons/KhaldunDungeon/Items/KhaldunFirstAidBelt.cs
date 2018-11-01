e4dee9ff13c41d5910e70dfbf972693a91146216
Dexter <kevin_bourne10@yahoo.com>
2018-10-31 17:18:10 -0700

    added Khaldun First Aid Belt

---
+++ Scripts/Services/Revamped Dungeons/KhaldunDungeon/Items/KhaldunFirstAidBelt.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Linq;
+
+namespace Server.Items
+{
+    public class KhaldunFirstAidBelt : FirstAidBelt
+    {
+        [Constructable]
+        public KhaldunFirstAidBelt()
+        {
+            WeightReduction = 50;
+            Attributes.RegenHits = 2;
+            HealingBonus = 10;
+        }
+
+        public KhaldunFirstAidBelt(Serial serial)
+            : base(serial)
+        {
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+            writer.Write(0); // version
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+            int version = reader.ReadInt();
+        }
+    }
+}
