134cb65d81448433208572f149784142119fb6b8
Dexter <kevin_bourne10@yahoo.com>
2018-11-10 21:58:16 -0700

    Scroll of Alacrity buff icon.

--- Scripts/Items/Consumables/ScrollofAlacrity.cs
+++ Scripts/Items/Consumables/ScrollofAlacrity.cs
@@ -42,8 +42,8 @@ namespace Server.Items
         public ScrollofAlacrity(SkillName skill)
             : base(skill, 0.0)
         {
-            this.ItemID = 0x14EF;
-            this.Hue = 1195;
+            ItemID = 0x14EF;
+            Hue = 1195;
         }
 
         public ScrollofAlacrity(Serial serial)
@@ -55,7 +55,7 @@ namespace Server.Items
         {
             base.GetProperties(list);
 
-            list.Add(1071345, "{0} 15 Minutes", this.GetName()); // Skill: ~1_val~
+            list.Add(1071345, "{0} 15 Minutes", GetName()); // Skill: ~1_val~
         }
 		
         public override bool CanUse(Mobile from)
@@ -68,7 +68,6 @@ namespace Server.Items
             if (pm == null)
                 return false;
 
-            #region Mondain's Legacy
             for (int i = pm.Quests.Count - 1; i >= 0; i--)
             {
                 BaseQuest quest = pm.Quests[i];
@@ -84,22 +83,19 @@ namespace Server.Items
                     }
                 }
             }
-            #endregion
 
-            #region Scroll of Alacrity
             if (pm.AcceleratedStart > DateTime.UtcNow)
             {
                 from.SendLocalizedMessage(1077951); // You are already under the effect of an accelerated skillgain scroll.
                 return false;
             }
-            #endregion
 			
             return true;
         }
 
         public override void Use(Mobile from)
         {
-            if (!this.CanUse(from))
+            if (!CanUse(from))
                 return;
 			
             PlayerMobile pm = from as PlayerMobile;
@@ -107,10 +103,10 @@ namespace Server.Items
             if (pm == null)
                 return;
 			
-            double tskill = from.Skills[this.Skill].Base;
-            double tcap = from.Skills[this.Skill].Cap;
+            double tskill = from.Skills[Skill].Base;
+            double tcap = from.Skills[Skill].Cap;
 
-            if (tskill >= tcap || from.Skills[this.Skill].Lock != SkillLock.Up)
+            if (tskill >= tcap || from.Skills[Skill].Lock != SkillLock.Up)
             {
                 from.SendLocalizedMessage(1094935);	/*You cannot increase this skill at this time. The skill may be locked or set to lower in your skill menu.
                 *If you are at your total skill cap, you must use a Powerscroll to increase your current skill cap.*/
@@ -128,9 +124,11 @@ namespace Server.Items
 
             m_Table[from] = Timer.DelayCall(TimeSpan.FromMinutes(15), new TimerStateCallback(Expire_Callback), from);
 
-            pm.AcceleratedSkill = this.Skill;
+            pm.AcceleratedSkill = Skill;
 
-            this.Delete();
+            BuffInfo.AddBuff(pm, new BuffInfo(BuffIcon.ArcaneEmpowerment, 1078511, 1078512, TimeSpan.FromMinutes(15), pm, GetName(), true));
+
+            Delete();
         }
 
         private static readonly Hashtable m_Table = new Hashtable();
@@ -169,10 +167,7 @@ namespace Server.Items
         {
             base.Deserialize(reader);
 
-            int version = (this.InheritsItem ? 0 : reader.ReadInt()); //Required for SpecialScroll insertion
-
-            this.LootType = LootType.Cursed;
-            this.Insured = false;
+            int version = (InheritsItem ? 0 : reader.ReadInt()); //Required for SpecialScroll insertion
         }
     }
 }
