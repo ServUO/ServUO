482f78e1892de3f0aa2003c6d7ae839e8c49d3d6
Dexter <kevin_bourne10@yahoo.com>
2018-12-16 09:36:57 -0700

    fixed Naxatilor spawners

--- Scripts/Mobiles/NPCs/Naxatillor.cs
+++ Scripts/Mobiles/NPCs/Naxatillor.cs
@@ -143,7 +143,7 @@ namespace Server.Engines.Quests
         {
             base.Serialize(writer);
 
-            writer.Write((int)0); // version
+            writer.Write((int)1); // version
         }
 
         public override void Deserialize(GenericReader reader)
@@ -151,6 +151,16 @@ namespace Server.Engines.Quests
             base.Deserialize(reader);
 
             int version = reader.ReadInt();
+
+            if (version == 0)
+            {
+                Timer.DelayCall(FixSpawner);
+            }
+        }
+
+        private void FixSpawner()
+        {
+            SpawnerPersistence.Replace("Naxatillor", "Naxatilor", null);
         }
     }
 }
