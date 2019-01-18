4b724e375981fc7ddd059402e0242015805b4ab2
Tasanar <knight.daniel.r@gmail.com>
2019-01-18 08:06:07 -0500

    Adds missing phoenix armor. Hue and clilocs are EA exact. Adds missing phoenix armor ticket. http://www.uoguide.com/File:Phoenixdeed.png Clilocs are EA exact.
    Fixed a bug where the new HABToken had the same graphic as the MysticalTotem in the UOStore.
    
    Moved the HAB token from Consumables folder to the StoreBought folder.

---
+++ Scripts/Items/Artifacts/Equipment/Armor/Sets/Phoenix/PhoenixGorget.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace Server.Items
+{
+    public class PhoenixGorget : StuddedGorget
+    {
+        [Constructable]
+        public PhoenixGorget()
+        {
+            Hue = 0x8E;
+			LootType = LootType.Blessed;     
+        }
+
+        public PhoenixGorget(Serial serial)
+            : base(serial)
+        {
+        }
+
+        public override int LabelNumber
+        {
+            get
+            {
+                return 1041604;
+            }
+        }// studded gorget of the phoenix
+        
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+
+            writer.Write((int)0);
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+
+            int version = reader.ReadInt();
+        }
+    }
+}
