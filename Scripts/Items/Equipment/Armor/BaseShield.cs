b6b07b95b805062ae0afac8eb33d8f1632f774c4
VitaNex <admin@vita-nex.com>
2019-05-12 17:41:03 +0100

    Arbitrary syntax updates.

--- Scripts/Items/Equipment/Armor/BaseShield.cs
+++ Scripts/Items/Equipment/Armor/BaseShield.cs
@@ -150,7 +150,7 @@ namespace Server.Items
                 if (chance < 0.01)
                     chance = 0.01;
                 /*
-                FORMULA: Displayed AR = ((Parrying Skill * Base AR of Shield) ÷ 200) + 1 
+                FORMULA: Displayed AR = ((Parrying Skill * Base AR of Shield) / 200) + 1 
 
                 FORMULA: % Chance of Blocking = parry skill - (shieldAR * 2)
 
