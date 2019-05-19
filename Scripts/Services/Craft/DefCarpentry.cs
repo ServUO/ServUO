ce667614a0152ce10243c4892c655ae6d032fab4
Tasanar <knight.daniel.r@gmail.com>
2019-05-19 13:33:06 -0400

    Using the wrong skill amounts for logs.
    Using the wrong skill amounts for logs.
    
    https://uo.com/wiki/ultima-online-wiki/skills/carpentry/

--- Scripts/Services/Craft/DefCarpentry.cs
+++ Scripts/Services/Craft/DefCarpentry.cs
@@ -998,11 +998,11 @@ namespace Server.Engines.Craft
             // This will override the overridable material	TODO: Verify the required skill amount
             AddSubRes(typeof(Board), 1072643, 00.0, 1044041, 1072652);
             AddSubRes(typeof(OakBoard), 1072644, 65.0, 1044041, 1072652);
-            AddSubRes(typeof(AshBoard), 1072645, 80.0, 1044041, 1072652);
-            AddSubRes(typeof(YewBoard), 1072646, 95.0, 1044041, 1072652);
-            AddSubRes(typeof(HeartwoodBoard), 1072647, 100.0, 1044041, 1072652);
-            AddSubRes(typeof(BloodwoodBoard), 1072648, 100.0, 1044041, 1072652);
-            AddSubRes(typeof(FrostwoodBoard), 1072649, 100.0, 1044041, 1072652);
+            AddSubRes(typeof(AshBoard), 1072645, 75.0, 1044041, 1072652);
+            AddSubRes(typeof(YewBoard), 1072646, 85.0, 1044041, 1072652);
+            AddSubRes(typeof(HeartwoodBoard), 1072647, 95.0, 1044041, 1072652);
+            AddSubRes(typeof(BloodwoodBoard), 1072648, 95.0, 1044041, 1072652);
+            AddSubRes(typeof(FrostwoodBoard), 1072649, 95.0, 1044041, 1072652);
         }
     }
 }
