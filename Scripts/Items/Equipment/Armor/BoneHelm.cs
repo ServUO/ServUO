487454331e973fd5704cc102828fde4eed88a95b
Tasanar <knight.daniel.r@gmail.com>
2019-08-09 12:46:21 -0400

    Cleanup of code.

--- Scripts/Items/Equipment/Armor/BoneHelm.cs
+++ Scripts/Items/Equipment/Armor/BoneHelm.cs
@@ -5,6 +5,19 @@ namespace Server.Items
     [FlipableAttribute(0x1451, 0x1456)]
     public class BoneHelm : BaseArmor
     {
+		public override int BasePhysicalResistance { get { return 3; } }
+        public override int BaseFireResistance { get { return 3; } }
+        public override int BaseColdResistance { get { return 4; } }
+        public override int BasePoisonResistance { get { return 2; } }
+        public override int BaseEnergyResistance { get { return 4; } }
+        public override int InitMinHits { get { return 25; } }
+        public override int InitMaxHits { get { return 30; } }
+        public override int AosStrReq { get { return 20; } }
+        public override int OldStrReq { get { return 40; } }
+        public override int ArmorBase { get { return 30; } }
+        public override ArmorMaterialType MaterialType { get { return ArmorMaterialType.Bone; } }
+		public override CraftResource DefaultResource { get { return CraftResource.RegularLeather; } }
+		
         [Constructable]
         public BoneHelm()
             : base(0x1451)
@@ -16,84 +29,7 @@ namespace Server.Items
             : base(serial)
         {
         }
-
-        public override int BasePhysicalResistance
-        {
-            get
-            {
-                return 3;
-            }
-        }
-        public override int BaseFireResistance
-        {
-            get
-            {
-                return 3;
-            }
-        }
-        public override int BaseColdResistance
-        {
-            get
-            {
-                return 4;
-            }
-        }
-        public override int BasePoisonResistance
-        {
-            get
-            {
-                return 2;
-            }
-        }
-        public override int BaseEnergyResistance
-        {
-            get
-            {
-                return 4;
-            }
-        }
-        public override int InitMinHits
-        {
-            get
-            {
-                return 25;
-            }
-        }
-        public override int InitMaxHits
-        {
-            get
-            {
-                return 30;
-            }
-        }
-        public override int AosStrReq
-        {
-            get
-            {
-                return 20;
-            }
-        }
-        public override int OldStrReq
-        {
-            get
-            {
-                return 40;
-            }
-        }
-        public override int ArmorBase
-        {
-            get
-            {
-                return 30;
-            }
-        }
-        public override ArmorMaterialType MaterialType
-        {
-            get
-            {
-                return ArmorMaterialType.Bone;
-            }
-        }
+        
         public override void Serialize(GenericWriter writer)
         {
             base.Serialize(writer);
