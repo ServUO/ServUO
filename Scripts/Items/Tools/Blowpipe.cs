545aed3245385e9c24311bcb3fb4b3b24e4f7281
Tasanar <knight.daniel.r@gmail.com>
2019-08-09 22:03:40 -0400

    More cleanup and name fix for Fletchers Tools. Confirmed on UO.

--- Scripts/Items/Tools/Blowpipe.cs
+++ Scripts/Items/Tools/Blowpipe.cs
@@ -6,36 +6,27 @@ namespace Server.Items
     [FlipableAttribute(0xE8A, 0xE89)]
     public class Blowpipe : BaseTool
     {
+		public override CraftSystem CraftSystem { get { return DefGlassblowing.CraftSystem; } }
         public override int LabelNumber { get { return 1044609; } } // Blow Pipe
 
         [Constructable]
         public Blowpipe()
             : base(0xE8A)
-        {
-            Weight = 1.0;
-            Hue = 2301;
+        {          
         }
 
         [Constructable]
         public Blowpipe(int uses)
             : base(uses, 0xE8A)
         {
-            Weight = 4.0;
+            Weight = 1.0;
             Hue = 0x3B9;
         }
 
         public Blowpipe(Serial serial)
             : base(serial)
         {
-        }
-
-        public override CraftSystem CraftSystem
-        {
-            get
-            {
-                return DefGlassblowing.CraftSystem;
-            }
-        }
+        }       
 
         public override void Serialize(GenericWriter writer)
         {
@@ -47,9 +38,6 @@ namespace Server.Items
         {
             base.Deserialize(reader);
             int version = reader.ReadInt();
-
-            if (Weight != 1.0)
-                Weight = 1.0;
         }
     }
 }
