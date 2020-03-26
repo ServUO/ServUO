01182e2af6721dc545fe00a0eb9783c097c7e790
TrueUO <knight.daniel.r@gmail.com>
2020-03-26 17:00:50 -0400

    Removes unneeded netstate checks. (#4625)

--- Scripts/Items/Internal/PaintedCavesTeleporters.cs
+++ Scripts/Items/Internal/PaintedCavesTeleporters.cs
@@ -17,12 +17,7 @@ namespace Server.Items
 
         public override bool OnMoveOver(Mobile m)
         {
-            if (m.NetState == null || !m.NetState.SupportsExpansion(Expansion.ML))
-            {
-                m.SendLocalizedMessage(1072608); // You must upgrade to the Mondain's Legacy expansion in order to enter here.				
-                return true;
-            }
-            else if (!MondainsLegacy.PaintedCaves && (int)m.AccessLevel < (int)AccessLevel.GameMaster)
+            if (!MondainsLegacy.PaintedCaves && (int)m.AccessLevel < (int)AccessLevel.GameMaster)
             {
                 m.SendLocalizedMessage(1042753, "Painted Caves"); // ~1_SOMETHING~ has been temporarily disabled.
                 return true;
