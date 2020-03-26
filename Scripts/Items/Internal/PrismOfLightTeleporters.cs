699122fdf94f79f26ce744a57c6d170481adde62
TrueUO <knight.daniel.r@gmail.com>
2020-03-26 14:11:35 -0400

    Removes unneeded netstate checks.

--- Scripts/Items/Internal/PrismOfLightTeleporters.cs
+++ Scripts/Items/Internal/PrismOfLightTeleporters.cs
@@ -17,12 +17,7 @@ namespace Server.Items
 
         public override bool OnMoveOver(Mobile m)
         {
-            if (m.NetState == null || !m.NetState.SupportsExpansion(Expansion.ML))
-            {
-                m.SendLocalizedMessage(1072608); // You must upgrade to the Mondain's Legacy expansion in order to enter here.				
-                return true;
-            }
-            else if (!MondainsLegacy.PrismOfLight && (int)m.AccessLevel < (int)AccessLevel.GameMaster)
+            if (!MondainsLegacy.PrismOfLight && (int)m.AccessLevel < (int)AccessLevel.GameMaster)
             {
                 m.SendLocalizedMessage(1042753, "Prism of Light"); // ~1_SOMETHING~ has been temporarily disabled.
                 return true;
