9c45ef8090b13932dfb792e5b18e304e37d19317
TrueUO <knight.daniel.r@gmail.com>
2020-04-12 00:26:45 -0400

    VirtualArmor cleanup

--- Scripts/Mobiles/Summons/SummonedAirElemental.cs
+++ Scripts/Mobiles/Summons/SummonedAirElemental.cs
@@ -9,38 +9,37 @@ namespace Server.Mobiles
         public SummonedAirElemental()
             : base(AIType.AI_Mage, FightMode.Closest, 10, 1, 0.2, 0.4)
         {
-            this.Name = "an air elemental";
-            this.Body = 13;
-            this.Hue = 0x4001;
-            this.BaseSoundID = 655;
+            Name = "an air elemental";
+            Body = 13;
+            Hue = 0x4001;
+            BaseSoundID = 655;
 
-            this.SetStr(200);
-            this.SetDex(200);
-            this.SetInt(100);
+            SetStr(200);
+            SetDex(200);
+            SetInt(100);
 
-            this.SetHits(150);
-            this.SetStam(50);
+            SetHits(150);
+            SetStam(50);
 
-            this.SetDamage(6, 9);
+            SetDamage(6, 9);
 
-            this.SetDamageType(ResistanceType.Physical, 50);
-            this.SetDamageType(ResistanceType.Energy, 50);
+            SetDamageType(ResistanceType.Physical, 50);
+            SetDamageType(ResistanceType.Energy, 50);
 
-            this.SetResistance(ResistanceType.Physical, 40, 50);
-            this.SetResistance(ResistanceType.Fire, 30, 40);
-            this.SetResistance(ResistanceType.Cold, 35, 45);
-            this.SetResistance(ResistanceType.Poison, 50, 60);
-            this.SetResistance(ResistanceType.Energy, 70, 80);
+            SetResistance(ResistanceType.Physical, 40, 50);
+            SetResistance(ResistanceType.Fire, 30, 40);
+            SetResistance(ResistanceType.Cold, 35, 45);
+            SetResistance(ResistanceType.Poison, 50, 60);
+            SetResistance(ResistanceType.Energy, 70, 80);
 
-            this.SetSkill(SkillName.Meditation, 90.0);
-            this.SetSkill(SkillName.EvalInt, 70.0);
-            this.SetSkill(SkillName.Magery, 70.0);
-            this.SetSkill(SkillName.MagicResist, 60.0);
-            this.SetSkill(SkillName.Tactics, 100.0);
-            this.SetSkill(SkillName.Wrestling, 80.0);
+            SetSkill(SkillName.Meditation, 90.0);
+            SetSkill(SkillName.EvalInt, 70.0);
+            SetSkill(SkillName.Magery, 70.0);
+            SetSkill(SkillName.MagicResist, 60.0);
+            SetSkill(SkillName.Tactics, 100.0);
+            SetSkill(SkillName.Wrestling, 80.0);
 
-            this.VirtualArmor = 40;
-            this.ControlSlots = 2;
+            ControlSlots = 2;
         }
 
         public SummonedAirElemental(Serial serial)
@@ -48,20 +47,10 @@ namespace Server.Mobiles
         {
         }
 
-        public override double DispelDifficulty
-        {
-            get
-            {
-                return 117.5;
-            }
-        }
-        public override double DispelFocus
-        {
-            get
-            {
-                return 45.0;
-            }
-        }
+        public override double DispelDifficulty => 117.5;
+
+        public override double DispelFocus => 45.0;
+
         public override void Serialize(GenericWriter writer)
         {
             base.Serialize(writer);
@@ -72,9 +61,6 @@ namespace Server.Mobiles
         {
             base.Deserialize(reader);
             int version = reader.ReadInt();
-
-            if (this.BaseSoundID == 263)
-                this.BaseSoundID = 655;
         }
     }
 }
