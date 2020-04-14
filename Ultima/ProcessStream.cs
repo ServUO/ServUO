aef5a7deb3611ac45297219e54457d9ac22e69dd
TrueUO <knight.daniel.r@gmail.com>
2020-04-14 11:35:04 -0400

    Ultima folder cleanup.

--- Ultima/ProcessStream.cs
+++ Ultima/ProcessStream.cs
@@ -5,109 +5,109 @@ using System.IO;
 
 namespace Ultima
 {
-	public abstract unsafe class ProcessStream : Stream
-	{
-		private const int ProcessAllAccess = 0x1F0FFF;
+    public abstract unsafe class ProcessStream : Stream
+    {
+        private const int ProcessAllAccess = 0x1F0FFF;
 
-		protected bool m_Open;
-		protected ClientProcessHandle m_Process;
+        protected bool m_Open;
+        protected ClientProcessHandle m_Process;
 
-		protected int m_Position;
+        protected int m_Position;
 
-		public abstract ClientProcessHandle ProcessID { get; }
+        public abstract ClientProcessHandle ProcessID { get; }
 
-		public virtual bool BeginAccess()
-		{
-			if (m_Open)
-			{
-				return false;
-			}
+        public virtual bool BeginAccess()
+        {
+            if (m_Open)
+            {
+                return false;
+            }
 
-			m_Process = NativeMethods.OpenProcess(ProcessAllAccess, 0, ProcessID);
-			m_Open = true;
+            m_Process = NativeMethods.OpenProcess(ProcessAllAccess, 0, ProcessID);
+            m_Open = true;
 
-			return true;
-		}
+            return true;
+        }
 
-		public virtual void EndAccess()
-		{
-			if (!m_Open)
-			{
-				return;
-			}
+        public virtual void EndAccess()
+        {
+            if (!m_Open)
+            {
+                return;
+            }
 
-			m_Process.Close();
-			m_Open = false;
-		}
+            m_Process.Close();
+            m_Open = false;
+        }
 
-		public override void Flush()
-		{ }
+        public override void Flush()
+        { }
 
-		public override int Read(byte[] buffer, int offset, int count)
-		{
-			bool end = !BeginAccess();
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            bool end = !BeginAccess();
 
-			int res = 0;
+            int res = 0;
 
-			fixed (byte* p = buffer)
-			{
-				NativeMethods.ReadProcessMemory(m_Process, m_Position, p + offset, count, ref res);
-			}
+            fixed (byte* p = buffer)
+            {
+                NativeMethods.ReadProcessMemory(m_Process, m_Position, p + offset, count, ref res);
+            }
 
-			m_Position += count;
+            m_Position += count;
 
-			if (end)
-			{
-				EndAccess();
-			}
+            if (end)
+            {
+                EndAccess();
+            }
 
-			return res;
-		}
+            return res;
+        }
 
-		public override void Write(byte[] buffer, int offset, int count)
-		{
-			bool end = !BeginAccess();
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            bool end = !BeginAccess();
 
-			fixed (byte* p = buffer)
-			{
-				NativeMethods.WriteProcessMemory(m_Process, m_Position, p + offset, count, 0);
-			}
+            fixed (byte* p = buffer)
+            {
+                NativeMethods.WriteProcessMemory(m_Process, m_Position, p + offset, count, 0);
+            }
 
-			m_Position += count;
+            m_Position += count;
 
-			if (end)
-			{
-				EndAccess();
-			}
-		}
+            if (end)
+            {
+                EndAccess();
+            }
+        }
 
-		public override bool CanRead { get { return true; } }
-		public override bool CanWrite { get { return true; } }
-		public override bool CanSeek { get { return true; } }
+        public override bool CanRead { get { return true; } }
+        public override bool CanWrite { get { return true; } }
+        public override bool CanSeek { get { return true; } }
 
-		public override long Length { get { throw new NotSupportedException(); } }
-		public override long Position { get { return m_Position; } set { m_Position = (int)value; } }
+        public override long Length { get { throw new NotSupportedException(); } }
+        public override long Position { get { return m_Position; } set { m_Position = (int)value; } }
 
-		public override void SetLength(long value)
-		{
-			throw new NotSupportedException();
-		}
+        public override void SetLength(long value)
+        {
+            throw new NotSupportedException();
+        }
 
-		public override long Seek(long offset, SeekOrigin origin)
-		{
-			switch (origin)
-			{
-				case SeekOrigin.Begin:
-					m_Position = (int)offset;
-					break;
-				case SeekOrigin.Current:
-					m_Position += (int)offset;
-					break;
-				case SeekOrigin.End:
-					throw new NotSupportedException();
-			}
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            switch (origin)
+            {
+                case SeekOrigin.Begin:
+                    m_Position = (int)offset;
+                    break;
+                case SeekOrigin.Current:
+                    m_Position += (int)offset;
+                    break;
+                case SeekOrigin.End:
+                    throw new NotSupportedException();
+            }
 
-			return m_Position;
-		}
-	}
+            return m_Position;
+        }
+    }
 }
