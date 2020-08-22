67f83b3ce8d0e326914032674813cbad9830c85e
Jaedan <jaedan@none.com>
2020-08-22 07:56:20 -0700

    Do not dynamically compile Scripts
    Scripts are now compiled into Scripts.dll and linked with Server.dll to
    form ServUO.exe. There is no compilation at run time.
    
    The build scripts will still function as normal but now require the
    'dotnet' tool which is available in the .NET core SDK. The project
    itself still builds against .NET 4.8.

--- Ultima/AssemblyInfo.cs
+++ Ultima/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.0.*")]
+[assembly: AssemblyVersion("2.0.0.0")]
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
 // Microsoft .NET Framework documentation for more information on assembly signing.
