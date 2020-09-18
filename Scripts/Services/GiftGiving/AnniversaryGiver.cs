10445e71243dd840878f9c7605021c409ade42b5
TrueUO <knight.daniel.r@gmail.com>
2020-09-18 14:54:02 -0400

    Holiday and Anniversary Gift Giving.

--- Scripts/Services/GiftGiving/AnniversaryGiver.cs
+++ Scripts/Services/GiftGiving/AnniversaryGiver.cs
@@ -3,20 +3,20 @@ using System;
 
 namespace Server.Misc
 {
-    public class AnniversaryGiver23rd : GiftGiver
+    public class AnniversaryGiver : GiftGiver
     {
         public static void Initialize()
         {
-            GiftGiving.Register(new AnniversaryGiver23rd());
+            GiftGiving.Register(new AnniversaryGiver());
         }
 
-        public override DateTime Start => new DateTime(2020, 09, 01);
-        public override DateTime Finish => new DateTime(2020, 10, 01);
-        public override TimeSpan MinimumAge => TimeSpan.FromDays(30);
+        public override DateTime Start => new DateTime(2019, 09, 01); // When do you want your gift hand out?
+        public override DateTime Finish => new DateTime(2019, 10, 01); // When do you want your gift hand out to stop?
+        public override TimeSpan MinimumAge => TimeSpan.FromDays(30); // How old does a character have to be to obtain?
 
         public override void GiveGift(Mobile mob)
         {
-            Item token = new Anniversary23GiftToken();
+            Item token = new Anniversary23GiftToken(); // Here you select your Anniversary Gift Pacakage.
 
             switch (GiveGift(mob, token))
             {
