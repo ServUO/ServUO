222a0ee9c5b4845481ee9a26650ff74eed47a979
Emil Johansson <p145085@users.noreply.github.com>
2020-10-28 13:27:22 +0100

    Servername change
    Changed servername to "chumbucket & Associates".

--- Scripts/Misc/ServerList.cs
+++ Scripts/Misc/ServerList.cs
@@ -43,7 +43,7 @@ namespace Server.Misc
 
         public static readonly bool AutoDetect = Config.Get("Server.AutoDetect", true);
 
-        public static string ServerName = Config.Get("Server.Name", "My Shard");
+        public static string ServerName = Config.Get("Server.Name", "chumbucket & Associates");
 
         private static IPAddress _PublicAddress;
 
