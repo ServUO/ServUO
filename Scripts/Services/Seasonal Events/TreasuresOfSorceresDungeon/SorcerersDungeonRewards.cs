fbb7b794113e742bf64caf85cd738b6647f21819
Dan <staff@trueuo.com>
2020-11-19 05:21:45 -0500

    Bug Fix
    - https://www.servuo.com/threads/sorcerers-dungeon-event-problem-with-artifact-of-the-cult-jack-in-the-box.13202/
    - Simply re-named it. None could spawn in the world previously.

--- Scripts/Services/Seasonal Events/TreasuresOfSorceresDungeon/SorcerersDungeonRewards.cs
+++ Scripts/Services/Seasonal Events/TreasuresOfSorceresDungeon/SorcerersDungeonRewards.cs
@@ -1,7 +1,5 @@
 using Server.Items;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Server.Engines.SorcerersDungeon
 {
@@ -15,19 +13,14 @@ namespace Server.Engines.SorcerersDungeon
 
             Rewards.Add(new CollectionItem(typeof(JacksBagOfTricks), 0x9F85, 1157656, 0, 75));
             Rewards.Add(new CollectionItem(typeof(SerpentsJawbone), 0x9F74, 1157654, 0, 50));
-            Rewards.Add(new CollectionItem(typeof(JackInTheBox), 0x9F64, 1157655, 0, 20));
-            Rewards.Add(new CollectionItem(typeof(ShackledHeartOfThePumpkinKing), 0x4A9C, 1157653, 0, 20)); // wtf does this do?
+            Rewards.Add(new CollectionItem(typeof(DecoJackInTheBox), 0x9F64, 1157655, 0, 20));
+            Rewards.Add(new CollectionItem(typeof(ShackledHeartOfThePumpkinKing), 0x4A9C, 1157653, 0, 20)); 
             Rewards.Add(new CollectionItem(typeof(HeroOfTheUnlovedTitleDeed), 0x14F0, 1157649, 0, 20));
             Rewards.Add(new CollectionItem(typeof(SaviorOfTheDementedTitleDeed), 0x14F0, 1157651, 0, 30));
             Rewards.Add(new CollectionItem(typeof(SlayerOfThePumpkinKingTitleDeed), 0x14F0, 1157650, 0, 50));
             Rewards.Add(new CollectionItem(typeof(SterlingSilverRing), 0x1F09, 1155606, 0, 50));
             Rewards.Add(new CollectionItem(typeof(TalonsOfEscaping), 0x41D8, 1155682, 0, 50));
             Rewards.Add(new CollectionItem(typeof(BootsOfEscaping), 0x1711, 1155607, 0, 50));
-        }
-
-        public static bool IsTokunoDyable(Type t)
-        {
-            return Rewards.FirstOrDefault(item => item.Type == t) != null;
         }
     }
 }
