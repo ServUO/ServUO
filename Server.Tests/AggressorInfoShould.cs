c1e359f93bf1b1b4ee51d4a2ebb419f4896c7e97
Michael Chekin <mchekin@gmail.com>
2021-03-17 17:16:55 +0100

    Adding Tests: re-organizing AggressorInfoShould.cs tests using fixture and constructor

--- Server.Tests/AggressorInfoShould.cs
+++ Server.Tests/AggressorInfoShould.cs
@@ -3,49 +3,58 @@ using Xunit;
 
 namespace Server.Tests
 {
-	public class AggressorInfoShould
+	public class AggressorInfoShould : IClassFixture<LoadWorldFixture>
 	{
+		private readonly Mobile _Attacker;
+		private readonly Mobile _Defender;
+
+		public AggressorInfoShould()
+		{
+			_Attacker = new Mobile();
+			_Defender = new Mobile();
+		}
+
 		[Fact]
 		public void HaveInitialState()
 		{
-			Server.Misc.MapDefinitions.Configure();
-			Server.World.Load();
-			
-			var attacker = new Mobile();
-			var defender = new Mobile();
+			const bool criminal = true;
 
-			var criminal = true;
-			
-			var sut = AggressorInfo.Create(attacker, defender, criminal);
-			
+			var sut = AggressorInfo.Create(_Attacker, _Defender, criminal);
+
 			Assert.False(sut.Reported);
 			Assert.Equal(criminal, sut.CriminalAggression);
 			Assert.False(sut.Expired);
 			Assert.Equal(criminal, sut.CanReportMurder);
-			Assert.Equal(attacker, sut.Attacker);
-			Assert.Equal(defender, sut.Defender); 
+			Assert.Equal(_Attacker, sut.Attacker);
+			Assert.Equal(_Defender, sut.Defender);
+			// ReSharper disable once RedundantTypeArgumentsOfMethod
 			Assert.InRange<DateTime>(sut.LastCombatTime, DateTime.UtcNow.AddSeconds(-5.0), DateTime.UtcNow);
 		}
 
-        [Fact]
-        public void CreateTheSameInstanceAfterCallingFree()
+		[Fact]
+		public void CreateTheSameInstanceAfterCallingFree()
 		{
-			Server.Misc.MapDefinitions.Configure();
-			Server.World.Load();
-			
-			var attacker = new Mobile();
-			var defender = new Mobile();
-			
-			var sut = AggressorInfo.Create(attacker, defender, true);
-			
-			var sut2 = AggressorInfo.Create(attacker, defender, true);
-			
+			var sut = AggressorInfo.Create(_Attacker, _Defender, true);
+			var sut2 = AggressorInfo.Create(_Attacker, _Defender, true);
+
 			sut.Free();
-			
-			var sut3 = AggressorInfo.Create(attacker, defender, true);
-			
+
+			var sut3 = AggressorInfo.Create(_Attacker, _Defender, true);
+
 			Assert.NotSame(sut, sut2);
 			Assert.Same(sut, sut3);
 		}
+	}
+
+	// ReSharper disable once ClassNeverInstantiated.Global
+	public class LoadWorldFixture : IDisposable
+	{
+		public LoadWorldFixture()
+		{
+			Misc.MapDefinitions.Configure();
+			World.Load();
+		}
+
+		public void Dispose() { }
 	}
 }
