bb831926180d6e524965d2ad197c83a6f13c70fb
tr1age <3319999+tr1age@users.noreply.github.com>
2023-03-27 17:29:21 -0400

    Death Ray does not use SDI on OSI
    By utilizing SDI on deathray you create a very overpowered Mastery Skill. OSI does not calculate SDI into the skill they only use Magery and Eval and Mastery level.

--- Scripts/Spells/Skill Masteries/DeathRay.cs
+++ Scripts/Spells/Skill Masteries/DeathRay.cs
@@ -121,10 +121,6 @@ namespace Server.Spells.SkillMasteries
                 damage /= Target is PlayerMobile ? 5.15 : 2.5;
 
                 damage *= GetDamageScalar(Target);
-
-                int sdiBonus = SpellHelper.GetSpellDamageBonus(Caster, Target, CastSkill, Caster.Player && Target.Player);
-
-                damage *= (100 + sdiBonus);
                 damage /= 100;
 
                 SpellHelper.Damage(this, Target, (int)damage, 0, 0, 0, 0, 100);
