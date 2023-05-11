9f93d8e814f8c32282a21b09037b1bacefbe241b
shroeder <deagle.shroeder@gmail.com>
2023-05-11 00:10:49 -0500

    add damage types to the book of masteries when discordance mastery is enabled

--- Scripts/Items/Equipment/Spellbooks/BookOfMasteries.cs
+++ Scripts/Items/Equipment/Spellbooks/BookOfMasteries.cs
@@ -1,131 +1,241 @@
+using System;
+using System.Collections.Generic;
 using Server.ContextMenus;
 using Server.Gumps;
 using Server.Mobiles;
 using Server.Spells.SkillMasteries;
 using Server.Spells.Spellweaving;
-using System;
-using System.Collections.Generic;
 
 namespace Server.Items
 {
-    [Flipable(0x225A, 0x225B)]
-    public class BookOfMasteries : Spellbook
-    {
-        public override SpellbookType SpellbookType => SpellbookType.SkillMasteries;
-        public override int BookOffset => 700;
-        public override int BookCount => 45;
+	[Flipable(0x225A, 0x225B)]
+	public class BookOfMasteries : Spellbook
+	{
+		public override SpellbookType SpellbookType => SpellbookType.SkillMasteries;
+		public override int BookOffset => 700;
+		public override int BookCount => 45;
 
-        private ulong _Content;
+		private ulong _Content;
 
-        [CommandProperty(AccessLevel.GameMaster)]
-        public new ulong Content
-        {
-            get
-            {
-                return _Content;
-            }
-            set
-            {
-                if (_Content != value)
-                {
-                    _Content = value;
+		[CommandProperty(AccessLevel.GameMaster)]
+		public new ulong Content
+		{
+			get
+			{
+				return _Content;
+			}
+			set
+			{
+				if (_Content != value)
+				{
+					_Content = value;
 
-                    InvalidateProperties();
-                }
-            }
-        }
+					InvalidateProperties();
+				}
+			}
+		}
 
-        [Constructable]
-        public BookOfMasteries() : this(0x1FFFFFFFFFFF)
-        {
-        }
+		[Constructable]
+		public BookOfMasteries() : this(0x1FFFFFFFFFFF)
+		{
+		}
 
-        [Constructable]
-        public BookOfMasteries(ulong content) : base(content, 0x225A)
-        {
-            _Content = content;
-        }
+		[Constructable]
+		public BookOfMasteries(ulong content) : base(content, 0x225A)
+		{
+			_Content = content;
+		}
 
-        public override void GetContextMenuEntries(Mobile from, List<ContextMenuEntry> list)
-        {
-            base.GetContextMenuEntries(from, list);
+		public override void GetContextMenuEntries(Mobile from, List<ContextMenuEntry> list)
+		{
+			base.GetContextMenuEntries(from, list);
+			var isDiscordanceMasteryActive = from.Skills.CurrentMastery == SkillName.Discordance;
 
-            SimpleContextMenuEntry menu = new SimpleContextMenuEntry(from, 1151948, m =>
-                {
-                    if (m is PlayerMobile && IsChildOf(m.Backpack) && CheckCooldown(m))
-                        BaseGump.SendGump(new MasterySelectionGump(m as PlayerMobile, this));
-                });
+			var switchMasteryClilocReference = 1151948;
 
-            if (!IsChildOf(from.Backpack) || !CheckCooldown(from))
-                menu.Enabled = false;
+			// Documentation says you can't change while the skill is active
+			// I don't know we really need that bit, seems like a qol improvement not having it
+			List<int> clilocReferences = new List<int>
+			{
+				switchMasteryClilocReference
+			};
 
-            list.Add(menu);
-        }
+			if (isDiscordanceMasteryActive)
+			{
+				clilocReferences.AddRange(new List<int>
+				{
+					1151800,
+					1151801,
+					1151802,
+					1151803,
+					1151804
+				});
+			}
 
-        private static Dictionary<Mobile, DateTime> m_Cooldown = new Dictionary<Mobile, DateTime>();
+			var currentElementType = GetDespairElementType(from);
 
-        public static void AddToCooldown(Mobile from)
-        {
-            if (m_Cooldown == null)
-                m_Cooldown = new Dictionary<Mobile, DateTime>();
+			clilocReferences.ForEach(clilocReference =>
+			{
+				var isSwitchMastery = clilocReference == switchMasteryClilocReference;
 
-            m_Cooldown[from] = DateTime.UtcNow + TimeSpan.FromMinutes(10);
-        }
+				SimpleContextMenuEntry menuEntry;
 
-        public static bool CheckCooldown(Mobile from)
-        {
-            if (from.AccessLevel > AccessLevel.Player)
-                return true;
+				if (isSwitchMastery)
+				{
+					menuEntry = new SimpleContextMenuEntry(from, clilocReference, m =>
+					{
+						if (m is PlayerMobile && IsChildOf(m.Backpack) && CheckCooldown(m))
+							BaseGump.SendGump(new MasterySelectionGump(m as PlayerMobile, this));
+					});
+				} else
+				{
+					var elementType = GetElementType(clilocReference);
+					var elementName = GetElementName(elementType);
+					var isEnabled = elementType != currentElementType;
+					menuEntry = new SimpleContextMenuEntry(from, clilocReference, m =>
+					{
+						if (m is PlayerMobile && IsChildOf(m.Backpack) && CheckCooldown(m))
+						{
+							UpdateDespairElementType(m, elementType);
+							m.SendMessage($"Despair Element Type set to: {elementName}");
+							PlaySwitchMasteryEffects(from);
+						}
+					}, -1, isEnabled);
+				}
 
-            if (m_Cooldown != null && m_Cooldown.ContainsKey(from))
-            {
-                if (m_Cooldown[from] < DateTime.UtcNow)
-                {
-                    m_Cooldown.Remove(from);
-                    return true;
-                }
+				if (!IsChildOf(from.Backpack) || !CheckCooldown(from))
+					menuEntry.Enabled = false;
 
-                return false;
-            }
+				list.Add(menuEntry);
+			});
+		}
 
-            return true;
-        }
+		private void PlaySwitchMasteryEffects(Mobile from)
+		{
+			from.PlaySound(0x0F5);
+			from.PlaySound(0x1ED);
 
-        public override void AddProperty(ObjectPropertyList list)
-        {
-            base.AddProperty(list);
+			from.FixedParticles(0x375A, 1, 30, 9966, 33, 2, EffectLayer.Head);
+			from.FixedParticles(0x37B9, 1, 30, 9502, 43, 3, EffectLayer.Head);
+		}
 
-            if (RootParent is Mobile)
-            {
-                SkillName sk = ((Mobile)RootParent).Skills.CurrentMastery;
+		private string GetElementName(ElementType type)
+		{
+			switch(type)
+			{
+				case ElementType.Fire:
+					return "Fire";
+				case ElementType.Poison:
+					return "Poison";
+				case ElementType.Energy:
+					return "Energy";
+				case ElementType.Cold:
+					return "Cold";
+				case ElementType.Physical:
+					return "Physical";
+				default:
+					return "Physical";
+			}
+		}
 
-                if (sk > 0)
-                {
-                    list.Add(MasteryInfo.GetLocalization(sk));
+		public static ElementType GetElementType(int clilocReference)
+		{
+			switch (clilocReference)
+			{
+				case 1151800:
+					return ElementType.Physical;
+				case 1151801:
+					return ElementType.Fire;
+				case 1151802:
+					return ElementType.Cold;
+				case 1151803:
+					return ElementType.Poison;
+				case 1151804:
+					return ElementType.Energy;
+				default:
+					return ElementType.Physical;
+			}
+		}
 
-                    if (sk == SkillName.Spellweaving)
-                    {
-                        list.Add(1060485, ArcanistSpell.GetMasteryFocusLevel((Mobile)RootParent).ToString()); // strength bonus ~1_val~
-                    }
-                }
-            }
-        }
+		private static Dictionary<Mobile, DateTime> m_Cooldown = new Dictionary<Mobile, DateTime>();
+		private static Dictionary<Mobile, ElementType> m_DespairElementType = new Dictionary<Mobile, ElementType>();
 
-        public BookOfMasteries(Serial serial)
-            : base(serial)
-        {
-        }
+		public static void UpdateDespairElementType(Mobile from, ElementType type)
+		{
+			m_DespairElementType[from] = type;
+		}
 
-        public override void Serialize(GenericWriter writer)
-        {
-            base.Serialize(writer);
-            writer.WriteEncodedInt(1); // version
-        }
+		public static ElementType GetDespairElementType(Mobile from)
+		{
+			ElementType element;
+			if (m_DespairElementType.TryGetValue(from, out element))
+				return element;
+			else
+				return ElementType.Physical;
+		}
 
-        public override void Deserialize(GenericReader reader)
-        {
-            base.Deserialize(reader);
-            int version = reader.ReadEncodedInt();
-        }
-    }
+		public static void AddToCooldown(Mobile from)
+		{
+			if (m_Cooldown == null)
+				m_Cooldown = new Dictionary<Mobile, DateTime>();
+
+			m_Cooldown[from] = DateTime.UtcNow + TimeSpan.FromMinutes(10);
+		}
+
+		public static bool CheckCooldown(Mobile from)
+		{
+			if (from.AccessLevel > AccessLevel.Player)
+				return true;
+
+			if (m_Cooldown != null && m_Cooldown.ContainsKey(from))
+			{
+				if (m_Cooldown[from] < DateTime.UtcNow)
+				{
+					m_Cooldown.Remove(from);
+					return true;
+				}
+
+				return false;
+			}
+
+			return true;
+		}
+
+		public override void AddProperty(ObjectPropertyList list)
+		{
+			base.AddProperty(list);
+
+			if (RootParent is Mobile)
+			{
+				SkillName sk = ((Mobile)RootParent).Skills.CurrentMastery;
+
+				if (sk > 0)
+				{
+					list.Add(MasteryInfo.GetLocalization(sk));
+
+					if (sk == SkillName.Spellweaving)
+					{
+						list.Add(1060485, ArcanistSpell.GetMasteryFocusLevel((Mobile)RootParent).ToString()); // strength bonus ~1_val~
+					}
+				}
+			}
+		}
+
+		public BookOfMasteries(Serial serial)
+			: base(serial)
+		{
+		}
+
+		public override void Serialize(GenericWriter writer)
+		{
+			base.Serialize(writer);
+			writer.WriteEncodedInt(1); // version
+		}
+
+		public override void Deserialize(GenericReader reader)
+		{
+			base.Deserialize(reader);
+			int version = reader.ReadEncodedInt();
+		}
+	}
 }
--- Scripts/Spells/Skill Masteries/BardSpells/Despair.cs
+++ Scripts/Spells/Skill Masteries/BardSpells/Despair.cs
@@ -1,163 +1,172 @@
-using Server.Targeting;
 using System;
+using Server.Items;
+using Server.Targeting;
 
 /*Target Strength Reduced by up to 32, 20 – 60 Damage (Physical), every 2 seconds, 
 affected by the target's magic resistance. (PvP and specific Mobs only).*/
 
 namespace Server.Spells.SkillMasteries
 {
-    public class DespairSpell : BardSpell
-    {
-        public static readonly string ModName = "Despair";
+	public class DespairSpell : BardSpell
+	{
+		public static readonly string ModName = "Despair";
 
-        private static readonly SpellInfo m_Info = new SpellInfo(
-                "Despair", "Kal Des Mani Tym",
-                -1,
-                9002
-            );
+		private static readonly SpellInfo m_Info = new SpellInfo(
+				"Despair", "Kal Des Mani Tym",
+				-1,
+				9002
+			);
 
-        public override double RequiredSkill => 90;
-        public override double UpKeep => 10;
-        public override int RequiredMana => 26;
-        public override bool PartyEffects => false;
-        public override SkillName CastSkill => SkillName.Discordance;
-        public override double SlayerBonus => 3.0;
+		public override double RequiredSkill => 90;
+		public override double UpKeep => 10;
+		public override int RequiredMana => 26;
+		public override bool PartyEffects => false;
+		public override SkillName CastSkill => SkillName.Discordance;
+		public override double SlayerBonus => 3.0;
 
-        private int m_StatMod;
-        private int m_Damage;
-        private int m_Rounds;
+		private int m_StatMod;
+		private int m_Damage;
+		private int m_Rounds;
 
-        public DespairSpell(Mobile caster, Item scroll) : base(caster, scroll, m_Info)
-        {
-        }
+		public DespairSpell(Mobile caster, Item scroll) : base(caster, scroll, m_Info)
+		{
+		}
 
-        public override void OnCast()
-        {
-            BardSpell spell = GetSpell(Caster, GetType()) as BardSpell;
+		public override void OnCast()
+		{
+			BardSpell spell = GetSpell(Caster, GetType()) as BardSpell;
 
-            if (spell != null)
-            {
-                spell.Expire();
-                Caster.SendLocalizedMessage(1115774); //You halt your spellsong.
-            }
-            else
-            {
-                Caster.Target = new InternalTarget(this);
-            }
-        }
+			if (spell != null)
+			{
+				spell.Expire();
+				Caster.SendLocalizedMessage(1115774); //You halt your spellsong.
+			}
+			else
+			{
+				Caster.Target = new InternalTarget(this);
+			}
+		}
 
-        public void OnTarget(Mobile m)
-        {
-            if (!Caster.CanSee(m))
-            {
-                Caster.SendLocalizedMessage(500237); // Target can not be seen.
-            }
-            else if (!m.Alive)
-            {
-                Caster.SendLocalizedMessage(1115773); // Your target is dead.
-            }
-            else if (Caster == m)
-            {
-                // TODO: Message?
-            }
-            else if (HasHarmfulEffects(m, GetType()))
-            {
-                Caster.SendLocalizedMessage(1115772); //Your target is already under the effect of this spellsong.
-            }
-            else if (CheckHSequence(m))
-            {
-                SpellHelper.Turn(Caster, m);
+		public void OnTarget(Mobile m)
+		{
+			if (!Caster.CanSee(m))
+			{
+				Caster.SendLocalizedMessage(500237); // Target can not be seen.
+			}
+			else if (!m.Alive)
+			{
+				Caster.SendLocalizedMessage(1115773); // Your target is dead.
+			}
+			else if (Caster == m)
+			{
+				Caster.SendMessage("This Spell cannot be cast on one's self...");
+			}
+			else if (HasHarmfulEffects(m, GetType()))
+			{
+				Caster.SendLocalizedMessage(1115772); //Your target is already under the effect of this spellsong.
+			}
+			else if (CheckHSequence(m))
+			{
+				SpellHelper.Turn(Caster, m);
 
-                Target = m;
-                //Caster.SendLocalizedMessage(1234567); //TODO: Message?
+				Target = m;
+				//Caster.SendLocalizedMessage(1234567); //TODO: Message?
 
-                HarmfulSpell(m);
+				HarmfulSpell(m);
 
-                m.FixedParticles(0x374A, 10, 15, 5028, EffectLayer.Waist);
+				m.FixedParticles(0x374A, 10, 15, 5028, EffectLayer.Waist);
 
-                int rounds = 5 + (int)(BaseSkillBonus * .75);
+				int rounds = 5 + (int)(BaseSkillBonus * .75);
 
-                m_StatMod = (int)((BaseSkillBonus * 2) + CollectiveBonus);
-                m_Damage = (int)((BaseSkillBonus * 4.5) + (CollectiveBonus * 2));
-                m_Rounds = 5 + (int)((BaseSkillBonus * .75) + (CollectiveBonus / 2));
+				m_StatMod = (int)((BaseSkillBonus * 2) + CollectiveBonus);
+				m_Damage = (int)((BaseSkillBonus * 4.5) + (CollectiveBonus * 2));
+				m_Rounds = 5 + (int)((BaseSkillBonus * .75) + (CollectiveBonus / 2));
 
-                string args = string.Format("{0}\t{1}", m_StatMod, m_Damage);
-                BuffInfo.AddBuff(m, new BuffInfo(BuffIcon.DespairTarget, 1115741, 1115743, args.ToString()));
-                BuffInfo.AddBuff(Caster, new BuffInfo(BuffIcon.DespairCaster, 1115741, 1115743, args.ToString()));
+				string args = string.Format("{0}\t{1}", m_StatMod, m_Damage);
+				BuffInfo.AddBuff(m, new BuffInfo(BuffIcon.DespairTarget, 1115741, 1115743, args.ToString()));
+				// This buff indicates physical damage, which is incorrect
+				BuffInfo.AddBuff(Caster, new BuffInfo(BuffIcon.DespairCaster, 1115741, 1115743, args.ToString())); 
 
-                BeginTimer();
-            }
+				BeginTimer();
+			}
 
-            FinishSequence();
-        }
+			FinishSequence();
+		}
 
-        public override void EndEffects()
-        {
-            BuffInfo.RemoveBuff(Target, BuffIcon.DespairTarget);
-            BuffInfo.RemoveBuff(Caster, BuffIcon.DespairCaster);
-        }
+		public override void EndEffects()
+		{
+			BuffInfo.RemoveBuff(Target, BuffIcon.DespairTarget);
+			BuffInfo.RemoveBuff(Caster, BuffIcon.DespairCaster);
+		}
 
-        public override void AddStatMods()
-        {
-            int offset = m_StatMod;
+		public override void AddStatMods()
+		{
+			int offset = m_StatMod;
 
-            if (Target != null)
-                Target.AddStatMod(new StatMod(StatType.Str, ModName, offset, TimeSpan.Zero));
-        }
+			if (Target != null)
+				Target.AddStatMod(new StatMod(StatType.Str, ModName, offset, TimeSpan.Zero));
+		}
 
-        public override void RemoveStatMods()
-        {
-            if (Target != null)
-                Target.RemoveStatMod(ModName);
-        }
+		public override void RemoveStatMods()
+		{
+			if (Target != null)
+				Target.RemoveStatMod(ModName);
+		}
 
-        public override bool OnTick()
-        {
-            bool tick = base.OnTick();
+		public override bool OnTick()
+		{
+			bool tick = base.OnTick();
 
-            if (Target == null || !Caster.InRange(Target.Location, PartyRange))
-                return false;
+			if (Target == null || !Caster.InRange(Target.Location, PartyRange))
+				return false;
 
-            int damage = m_Damage;
+			int damage = m_Damage;
 
-            if (!Target.Player)
-                damage += AOS.Scale(damage, 50); // pvm = 1.5x
+			if (!Target.Player)
+				damage += AOS.Scale(damage, 50); // pvm = 1.5x
 
-            damage = (int)(damage * GetSlayerBonus()); // 3.0x slayer bonus
-            damage -= (int)(damage * DamageModifier(Target)); // resist modifier
+			damage = (int)(damage * GetSlayerBonus()); // 3.0x slayer bonus
+			damage -= (int)(damage * DamageModifier(Target)); // resist modifier
 
-            AOS.Damage(Target, Caster, damage, 100, 0, 0, 0, 0); // Now only does physical
+			var elementType = BookOfMasteries.GetDespairElementType(Caster);
+			var physicalDamage = elementType == Mobiles.ElementType.Physical ? 100 : 0;
+			var fireDamage = elementType == Mobiles.ElementType.Fire ? 100 : 0;
+			var poisonDamage = elementType == Mobiles.ElementType.Poison ? 100 : 0;
+			var energyDamage = elementType == Mobiles.ElementType.Energy ? 100 : 0;
+			var coldDamage = elementType == Mobiles.ElementType.Cold ? 100 : 0;
 
-            if (Target != null && Target.Alive && Target.Map != null)
-                Target.FixedEffect(0x376A, 1, 32);
+			AOS.Damage(Target, Caster, damage, physicalDamage, fireDamage, coldDamage, poisonDamage, energyDamage);
 
-            if (m_Rounds-- == 0)
-            {
-                Expire();
-            }
+			if (Target != null && Target.Alive && Target.Map != null)
+				Target.FixedEffect(0x376A, 1, 32);
 
-            return tick;
-        }
+			if (m_Rounds-- == 0)
+			{
+				Expire();
+			}
 
-        private class InternalTarget : Target
-        {
-            private readonly DespairSpell m_Owner;
+			return tick;
+		}
 
-            public InternalTarget(DespairSpell spell) : base(10, false, TargetFlags.Harmful)
-            {
-                m_Owner = spell;
-            }
+		private class InternalTarget : Target
+		{
+			private readonly DespairSpell m_Owner;
 
-            protected override void OnTarget(Mobile from, object o)
-            {
-                if (o is Mobile)
-                    m_Owner.OnTarget((Mobile)o);
-            }
+			public InternalTarget(DespairSpell spell) : base(10, false, TargetFlags.Harmful)
+			{
+				m_Owner = spell;
+			}
 
-            protected override void OnTargetFinish(Mobile from)
-            {
-                m_Owner.FinishSequence();
-            }
-        }
-    }
+			protected override void OnTarget(Mobile from, object o)
+			{
+				if (o is Mobile)
+					m_Owner.OnTarget((Mobile)o);
+			}
+
+			protected override void OnTargetFinish(Mobile from)
+			{
+				m_Owner.FinishSequence();
+			}
+		}
+	}
 }
